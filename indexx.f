860e9d8c1ac7283872251d5a94a37ba3112b57f5
Joe Taylor <k1jt@arrl.org>
2005-12-29 20:31:33 +0000

    Added indexx.f
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/trunk@35 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

---
+++ indexx.f
@@ -0,0 +1,19 @@
+      subroutine indexx(n,arr,indx)
+
+      parameter (NMAX=2000)
+      integer indx(n)
+      real arr(n)
+      real brr(NMAX)
+      if(n.gt.NMAX) then
+         print*,'n=',n,' too big in indexx.'
+         stop
+      endif
+      do i=1,n
+         brr(i)=arr(i)
+         indx(i)=i
+      enddo
+      call ssort(brr,indx,n,2)
+
+      return
+      end
+
