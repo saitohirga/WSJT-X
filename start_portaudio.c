36fcb937e385cfd452c901d62d338a395d3ae2a5
Diane Bruce <db@db.net>
2006-01-14 15:47:35 +0000

    - change from start_threads.c to start_alsa.c start_portaudio.c etc. - split start_threads.c into start_alsa.c and start_portaudio.c - make configure/Makefile add start_alsa or start_portaudio as needed   instead of start_threads
      Untested on ALSA, works fine with PORTAUDIO
    
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/trunk@100 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

---
+++ start_portaudio.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <inttypes.h>
+#include <time.h>
+#include "conf.h"
+
+extern void decode1_(int *iarg);
+extern void a2d_(int *iarg);
+
+int start_threads_(int *ndevin, int *ndevout, short y1[], short y2[],
+	int *nbuflen, int *iwrite, short iwave[],
+	int *nwave, int *nfsample, int *nsamperbuf,
+	int *TRPeriod, int *TxOK, int *ndebug,
+	int *Transmitting, double *Tsec, int *ngo, int *nmode,
+	double tbuf[], int *ibuf, int *ndsec)
+{
+  pthread_t thread1,thread2;
+  int iret1,iret2;
+  int iarg1 = 1,iarg2 = 2;
+
+ /* snd_pcm_start */
+  //  printf("start_threads: creating thread for a2d\n");
+  iret1 = pthread_create(&thread1,NULL,a2d_,&iarg1);
+  //  printf("start_threads: creating thread for decode1_\n");
+  iret2 = pthread_create(&thread2,NULL,decode1_,&iarg2);
+}
