eea8944eebbd7e3df614d0e69174b2f094d655e0
Joe Taylor <k1jt@arrl.org>
2006-04-21 19:33:30 +0000

    More changes to support overlong calls like VU4AN/VU3RYH.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/trunk@178 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- chkmsg.f
+++ chkmsg.f
@@ -11,17 +11,17 @@
       enddo
       i=22
 
- 10   if(i.ge.11 .and. message(i-3:i).eq.' OOO') then
+ 10   if(i.ge.11 .and. (message(i-3:i).eq.' OOO') .or. 
+     +    (message(20:22).eq.' OO')) then
          cok='OOO'
          flip=-1.0
          message=message(1:i-4)
       endif
 
-      if(message(1:3).eq.'ATT') nspecial=1
+!      if(message(1:3).eq.'ATT') nspecial=1
       if(message(1:2).eq.'RO')  nspecial=2
       if(message(1:3).eq.'RRR') nspecial=3
       if(message(1:2).eq.'73')  nspecial=4
 
       return
       end
-
