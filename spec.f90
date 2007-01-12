e375b9baae3fbf1d73b9ba7479021b1ade739e42
Joe Taylor <k1jt@arrl.org>
2007-01-12 19:56:45 +0000

    Basic waterfall now works.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/map65@346 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- spec.f90
+++ spec.f90
@@ -10,68 +10,59 @@ subroutine spec(brightness,contrast,logmap,ngain,nspeed,a)
 ! Output:
   integer*2 a(NTOT)             !Pixel values for NX x NY array
 
-!  real a0(NTOT)                 !Save the last NY spectra
-  integer nstep(5)
+  logical first
+  integer nstep(4)
   integer b0,c0
-  real s(NFFT)
+  real s(NX,NY)
   common/spcom/ss(4,322,NFFT)                !169 MB: half-symbol spectra
   include 'gcom1.f90'
   include 'gcom2.f90'
   include 'gcom3.f90'
   include 'gcom4.f90'
-  data jz/0/                    !Number of spectral lines available
-  data nstep/15,10,5,2,1/       !Integration limits
+  data first/.true./
+  data nstep/16,10,5,3/       !Integration limits
   save
 
-  df=96000.0/nfft
-  b0=-999
-  c0=-999
-  logmap0=-999
-  nspeed0=-999
-  nmode=2                                  !JT65 mode
+  if(first) then
+     df=96000.0/nfft
+     call zero(a,NX*NY/2)
+     first=.false.
+  endif
 
   nadd=nstep(nspeed)
   nlines=322/nadd
-  j=0
-  print*,'A',nspeed,nadd,nlines
-  do k=1,nlines
-     do i=1,nfft
-        s(i)=0.
-        do n=1,nadd
-           j=j+1
-           s(i)=s(i) + ss(1,j,i) + ss(3,j,i)
-        enddo
-        s(i)=s(i)/nadd
-     enddo
-     print*,'B',k
-
-!  Compute pixel values 
-!     iz=NX
-!     logmap=0
-!     if(brightness.ne.b0 .or. contrast.ne.c0 .or. logmap.ne.logmap0 .or.    &
-!          nspeed.ne.nspeed0 .or. nlines.gt.1) then
-     iz=NTOT
-     gain=40*sqrt(nstep(nspeed)/5.0) * 5.0**(0.01*contrast)
-     gamma=1.3 + 0.01*contrast
-     offset=(brightness+64.0)/2
-     b0=brightness
-     c0=contrast
-     logmap0=logmap
-     nspeed0=nspeed
-     ia=10001
-     ib=10750
-
+  call zero(s,NX*NY)
+  k=0
+  ia=9001
+  ib=9750
+  do j=1,nlines
+     k=k+1
      do i=ia,ib
-        n=0
-        if(s(i).gt.0.0 .and. logmap.eq.1) n=gain*log10(0.001*s(i)) &
-             + offset + 20
-        if(s(i).gt.0.0 .and. logmap.eq.0) n=(0.01*s(i))**gamma + offset
-        n=min(252,max(0,n))
-        a(i)=n
+        s(i,j)=s(i,j) + ss(1,k,i) + ss(3,k,i)
      enddo
-     print*,'C'
   enddo
-  print*,'D'
+
+  newpts=NX*nlines
+  do i=newpts+1,NX*NY
+     a(i)=a(i-newpts)
+  enddo
+
+  gain=40*sqrt(nstep(nspeed)/5.0) * 5.0**(0.01*contrast)
+  gamma=1.3 + 0.01*contrast
+  offset=(brightness+64.0)/2
+  k=0
+  do j=1,nlines
+     do i=ia,ib
+        k=k+1
+        n=0
+        x=s(i,j)
+!        if(s(i).gt.0.0 .and. logmap.eq.1) n=gain*log10(0.001*s(i)) &
+!             + offset + 20
+        if(x.gt.0.0 .and. logmap.eq.0) n=(20.0*x)**gamma + offset
+        n=min(252,max(0,n))
+        a(k)=n
+     enddo
+  enddo
 
   return
 end subroutine spec
