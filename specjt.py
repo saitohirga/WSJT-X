6875afc7711269427197092c0afc8902f0d988a6
Joe Taylor <k1jt@arrl.org>
2007-07-04 17:44:39 +0000

    Fix "Erase Band Map and Messages." Clean up File menu. Reduce shirthand signal levels by 6 dB. Anchor text to W in callsign readout label.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/map65@470 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- specjt.py
+++ specjt.py
@@ -380,9 +380,9 @@ setupmenu.add_radiobutton(label='Hot',command=pal_Hot,
 setupmenu.add_radiobutton(label='AFMHot',command=pal_AFMHot,
             value=5,variable=npal)
 
-lab1=Label(mbar,padx=20,bd=0)
+lab1=Label(mbar,width=20,padx=20,bd=0)
 lab1.pack(side=LEFT)
-fdf=Label(mbar,width=30,bd=0)
+fdf=Label(mbar,width=30,bd=0,anchor=W)
 fdf.pack(side=LEFT)
 fdf2=Label(mbar,width=15,bd=0)
 fdf2.pack(side=LEFT)
