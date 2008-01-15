8540823dfd7a66fa39483b3dba7f505e37cad5d1
Joe Taylor <k1jt@arrl.org>
2008-01-15 00:06:31 +0000

    Trivial deletion in astro0.F90.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/map65@622 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- astro0.F90
+++ astro0.F90
@@ -95,7 +95,6 @@ subroutine astro0(nyear,month,nday,uth8,nfreq,grid,cauxra,cauxdec,       &
   imin=60*uth8
   isec=3600*uth8
 
-!#ifdef CVF
   if(isec.ne.isec0 .and. ndecoding.eq.0) then
      ih=uth8
      im=mod(imin,60)
@@ -114,7 +113,6 @@ subroutine astro0(nyear,month,nday,uth8,nfreq,grid,cauxra,cauxdec,       &
      rewind 14
      isec0=isec
   endif
-!#endif
 
   return
 end subroutine astro0
