c6c48cbbd76b164f62015f27ee5b3de4727064be
Joe Taylor <k1jt@arrl.org>
2009-09-19 12:59:00 +0000

    Test for >13 char message moved from tx6 to tx5.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/map65@1405 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- map65.py
+++ map65.py
@@ -1110,8 +1110,7 @@ def update():
     tx4.configure(bg='white')
     tx5.configure(bg='white')
     tx6.configure(bg='white')
-    if tx6.get()[:3] != 'CQ ' and len(tx6.get())>13:
-        tx6.configure(bg='pink')
+    if len(tx7.get())>13: tx5.configure(bg='pink')
 
     if tx6.get()[:1]=='#':
         try:
