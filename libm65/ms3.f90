8c550223d1f105488119729c9cf10a8bb3dfcca9
Joe Taylor <k1jt@arrl.org>
2012-07-09 16:00:08 +0000

    Working on stand-alone decoder, ms3.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/jtms3@2502 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- libm65/ms3.f90
+++ libm65/ms3.f90
@@ -3,13 +3,9 @@ program ms3
 ! Starting code for a JTMS3 decoder.
 
   character*80 infile
-  parameter (NSMAX=30*48000)
-  parameter (NFFT=8192,NH=NFFT/2)
   integer hdr(11)
-  integer*2 id(NSMAX)
-  real x(NFFT)
-  complex cx(NFFT),cx2(NFFT)
-  real s(NH)
+  integer*2 id
+  common/mscom/id(1440000),s1(215,703),s2(215,703)
 
   nargs=iargc()
   if(nargs.lt.1) then
@@ -18,9 +14,9 @@ program ms3
      go to 999
   endif
 
-  npts=NSMAX
-  kstep=1024
-  nsteps=npts/kstep
+  npts=30*48000
+  kstep=4096
+
   do ifile=1,nargs
      call getarg(ifile,infile)
      open(10,file=infile,access='stream',status='old',err=998)
@@ -28,46 +24,9 @@ program ms3
      read(10) id
      close(10)
 
-     k=hdr(1)
-     k=0
-     do j=1,nsteps
-        sq=0.
-        do i=1,kstep
-           k=k+1
-           sq=sq + (0.001*id(k))**2
-        enddo
-        t=j*kstep/48000.0
-        pdb=db(sq)
-        write(13,1010) t,sq,pdb
-1010    format(3f12.3)
+     do k=kstep,npts,kstep
+        call specjtms(k)
      enddo
-  enddo
-
-  iz=29.5*48000.0
-  df=48000.0/nfft
-  ja=nint(2600.0)/df
-  jb=nint(3400.0)/df
-  do i=1,iz,nh
-     x(1:nfft)=0.001*id(i:i+nfft-1)
-     call analytic(x,nfft,nfft,s,cx)
-     t=i/48000.0
-     sq=dot_product(x,x) 
-     write(14,1010) t,sq,db(sq)
-     cx2=cx*cx
-     call four2a(cx2,nfft,1,-1,1)               !Forward c2c FFT
-     smax=0.
-     do j=ja,jb
-        sq=1.e-6*(real(cx2(j))**2 + aimag(cx2(j))**2)
-        f=(j-1)*df
-        if(sq.gt.smax) then
-           smax=sq
-           xdf=0.5*(f-3000.0)
-        endif
-        write(15,1020) (j-1)*df,sq
-1020    format(f10.3,f12.3)
-     enddo
-     write(16,1030) t,smax,xdf
-1030 format(3f12.3)
   enddo
 
   go to 999
