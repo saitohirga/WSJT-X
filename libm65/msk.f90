5079669e49cd20adbf9fa6dc376933b3a7d24f85
Joe Taylor <k1jt@arrl.org>
2012-07-19 18:01:14 +0000

    Now have a properly functioning stand-along decoder for JTMSK.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/jtms3@2529 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- libm65/msk.f90
+++ libm65/msk.f90
@@ -1,11 +1,12 @@
 program msk
 
-! Starting code for a JTMSK decoder.
+! Program to test decoding routines for mode JTMSK.
 
   parameter (NSMAX=30*48000)
   character*80 infile
   character*6 cfile6
   character*12 arg
+  character*12 mycall
   real dat(NSMAX)
   real x(NSMAX)
   complex cx(0:NSMAX/2)
@@ -14,46 +15,49 @@ program msk
   common/mscom/id(NSMAX),s1(215,703),s2(215,703)
 
   nargs=iargc()
-  if(nargs.lt.1) then
-     print*,'Usage: msk <snr>'
+  if(nargs.lt.2) then
+     print*,'Usage: msk nslow snr'
      go to 999
   endif
   call getarg(1,arg)
+  read(arg,*) nslow
+  call getarg(2,arg)
   read(arg,*) snr
 
+! Read simulated pings from a file
   open(71,file='dat.71',form='unformatted',status='old')
   read(71) id
-  cfile6='123400'
 
+  cfile6='123400'
   npts=30*48000
   kstep=2048
-  minsigdb=6
+  minsigdb=1
   mousedf=0
   ntol=200 
+  mycall='W8WN'
 
+! Make some band-limited noise.
   call random_number(x)
   nfft=NSMAX
   call four2a(x,nfft,1,-1,0)
   df=48000.0/nfft
   ia=nint(300.0/df)
-  ib=nint(2800.0/df)
+  ib=nint(2700.0/df)
   cx(:ia)=0.
   cx(ib:)=0.
   call four2a(cx,nfft,1,1,-1)
   x(1)=0.
-  sq=0.
-  do i=1,NSMAX
-     sq=sq + x(i)**2
-  enddo
-  rms=sqrt(sq/NSMAX)
+  rms=sqrt(dot_product(x,x)/NSMAX)
   x=x/rms
-  sig=(10.0**(0.05*snr))/32768.0
-  dat=sig*id + x
 
-  k=0
+  sig=(10.0**(0.05*snr))/32768.0                    !Scaled signal strength
+  dat=sig*id + x                                    !Add pings to noise
+
+! This loop simulates being called from "datasink()" in program JTMSK.
   do iblk=1,npts/kstep
-     k=k+kstep
-     call rtping(dat,k,cfile6,MinSigdB,MouseDF,ntol)
+     k=iblk*kstep
+     call rtping(dat,k,cfile6,MinSigdB,MouseDF,ntol,mycall)
+     if(nslow.ne.0) call usleep(42000)
   enddo
 
 999 end program msk
