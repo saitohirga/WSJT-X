8a3b15aba91519a8891f4c3e05d09a6824e50c18
Joe Taylor <k1jt@arrl.org>
2012-09-28 19:42:00 +0000

    More work on jt9sim.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@2604 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- libm65/genjt9.f90
+++ libm65/genjt9.f90
@@ -1,26 +1,27 @@
-subroutine genjt9(message,minutes,lwave,msgsent,d8,iwave,nwave)
+subroutine genjt9(message,minutes,msgsent,d6)
 
-! Encodes a "JT9-minutes" message and returns array d7(85) of tone
-! values in the range 0-8.  If lwave is true, also returns a generated
-! waveform in iwave(nwave), at 12000 samples per second.
+! Encodes a "JT9-minutes" message and returns array d6(85) of tone
+! values in the range 0-8.  
 
-  parameter (NMAX=1800*12000)   !Max length of wave file
   character*22 message          !Message to be generated
   character*22 msgsent          !Message as it will be received
-  real*8 dt,phi,f,f0,dfgen,dphi,twopi
-  integer*2 iwave(NMAX)         !Generated wave file
 
-  integer*4 d0(12)              !72-bit message as 6-bit words
-  integer*1 d1(72)              !72 single bits 
-  integer*4 d2(9)               !72 bits as 8-bit words
-  integer*1 d3(9)               !72 bits as 8-bit bytes
-  integer*1 d4(206)             !Encoded information-carrying bits
-  integer*1 d5(206)             !Bits from d4, after interleaving
-  integer*4 d6(69)              !Symbols from d5, values 0-7
-  integer*4 d7(69)              !Gray-coded symbols, values 0-7
-  integer*4 d8(85)              !Channel symbols including sync, values 0-8
+  integer*4 d0(13)              !72-bit message as 6-bit words
+  integer*1 d1(13)              !72 bits and zero tail as 8-bit bytes
+  integer*1 d2(206)             !Encoded information-carrying bits
+  integer*1 d3(206)             !Bits from d2, after interleaving
+  integer*4 d4(69)              !Symbols from d3, values 0-7
+  integer*4 d5(69)              !Gray-coded symbols, values 0-7
+  integer*4 d6(85)              !Channel symbols including sync, values 0-8
 
-  logical lwave
+  integer*4 t0(13)              !72-bit message as 6-bit words
+  integer*1 t1(13)              !72 bits and zero tail as 8-bit bytes
+  integer*1 t2(206)             !Encoded information-carrying bits
+  integer*1 t3(206)             !Bits from d2, after interleaving
+  integer*4 t4(69)              !Symbols from d3, values 0-7
+  integer*4 t5(69)              !Gray-coded symbols, values 0-7
+  integer*4 t6(85)              !Channel symbols including sync, values 0-8
+
   integer isync(85)
   data isync/                                    &
        1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,  &
@@ -31,67 +32,38 @@ subroutine genjt9(message,minutes,lwave,msgsent,d8,iwave,nwave)
   data twopi/6.283185307179586476d0/
   save
 
-  call packmsg(message,d0)            !Pack message into 12 6-bit bytes
-  call unpackbits(d0,12,6,d1)         !Unpack into 72 bits
-
-  nbits=72
-  nbytes=9
-  call packbits(d1,nbits,8,d2)        !Pack into 9 8-bit words
-  do i=1,9
-     if(d2(i).lt.128) d3(i)=d2(i)
-     if(d2(i).ge.128) d3(i)=d2(i)-256
-  enddo
-
-  call encode232(d3,nbytes,d4)        !Convolutional code, K=32, r=1/2
+  call packmsg(message,d0)           !Pack message into 12 6-bit bytes
+  call unpackmsg(d0,msgsent)
+  call entail(d0,d1)
   nsym2=206
-  call interleave9(d4,1,d5)
-  call packbits(d5,nsym2,3,d6)
-  call graycode(d6,69,1,d7)
+  call encode232(d1,nsym2,d2)        !Convolutional code, K=32, r=1/2
+  call interleave9(d2,1,d3)
+  call packbits(d3,nsym2,3,d4)
+  call graycode(d4,69,1,d5)
 
 ! Insert sync symbols (ntone=0) and add 1 to the data-tone numbers.
   j=0
   do i=1,85
      if(isync(i).eq.1) then
-        d8(i)=0
+        d6(i)=0
      else
         j=j+1
-        d8(i)=d7(j)+1
+        d6(i)=d5(j)+1
      endif
   enddo
 
-  nwave=0
+!  t6=d6
+!  j=0
+!  do i=1,85
+!     if(isync(i).eq.1) cycle
+!     j=j+1
+!     t5(j)=t6(i)-1
+!  enddo
 
-  if(lwave) then
-     nsps=0
-     if(minutes.eq.1)  nsps=6912
-     if(minutes.eq.2)  nsps=15360
-     if(minutes.eq.5)  nsps=40960
-     if(minutes.eq.10) nsps=82944
-     if(minutes.eq.30) nsps=250880
-     if(nsps.eq.0) stop 'Bad value for minutes in genjt9.'
-
-! Set up necessary constants
-     dt=1.d0/12000.d0
-     f0=1500.d0
-     dfgen=12000.d0/nsps
-     phi=0.d0
-     i=0
-     k=0
-     do j=1,85
-        f=f0 +d7(j)*dfgen
-        dphi=twopi*dt*f
-        do i=1,nsps
-           phi=phi+dphi
-           if(phi.gt.twopi) phi=phi-twopi
-           xphi=phi
-           k=k+1
-           iwave(k)=32767.0*sin(xphi)
-        enddo
-     enddo
-     nwave=85*nsps
-  endif
-
-  call unpackmsg(d0,msgsent)
+!  call graycode(t5,69,-1,t4)
+!  call unpackbits(t4,nsym2,3,t3)
+!  call interleave9(t3,-1,t2)
+!  print*,'A',d2-t2
 
   return
 end subroutine genjt9
--- libm65/jt9sim.f90
+++ libm65/jt9sim.f90
@@ -7,8 +7,7 @@ program jt9sim
   integer*2 iwave                     !Generated waveform (no noise)
   real*8 f0,f,dt,twopi,phi,dphi,baud,fspan
   character msg0*22,message*22,msgsent*22,arg*8,fname*11
-  logical lwave
-  integer*4 d8(85)
+  integer*4 d6(85)
   common/acom/dat(NMAX),iwave(NMAX)
 
   nargs=iargc()
@@ -41,7 +40,6 @@ program jt9sim
   dt=1.d0/fsample                    !Sample interval (s)
   twopi=8.d0*atan(1.d0)
   npts=12000*(60*minutes-6)
-  lwave=.false.
   nsps=0
   if(minutes.eq.1)  nsps=6912
   if(minutes.eq.2)  nsps=15360
@@ -72,7 +70,7 @@ program jt9sim
      endif
 
      if(msg0.ne.'                      ') then
-        call genjt9(message,minutes,lwave,msgsent,d8,iwave,nwave)
+        call genjt9(message,minutes,msgsent,d6)
      endif
 
      rewind 12
@@ -81,7 +79,7 @@ program jt9sim
         if(msg0.eq.'                      ') then
            read(12,1004) message
 1004       format(a22)
-           call genjt9(message,minutes,lwave,msgsent,d8,iwave,nwave)
+           call genjt9(message,minutes,msgsent,d6)
         endif
 
         f=f0
@@ -96,7 +94,7 @@ program jt9sim
         baud=12000.0/nsps
         k=12000                             !Start at t = 1 s
         do isym=1,85
-           freq=f + d8(isym)*baud
+           freq=f + d6(isym)*baud
            dphi=twopi*freq*dt
            do i=1,nsps
               phi=phi + dphi
@@ -111,18 +109,10 @@ program jt9sim
 
      do i=1,npts
         iwave(i)=nint(rms*dat(i))
-!        iwave(i)=nint(100.0*dat(i))
      enddo
 
      write(10) ihdr,iwave(1:npts)
      close(10)
-
-     
-     do i=1,30000
-        write(71,3001) i,iwave(12000+i)
-3001    format(2i8)
-     enddo
-
   enddo
 
 999 end program jt9sim
