78610586fca501354b8f8c22aaf52bea4afe7b70
Joe Taylor <k1jt@arrl.org>
2012-11-15 23:40:57 +0000

    Add nlim to decoded text lines in wsjtx_rx.log. Increase amplitude of red curve.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@2736 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- lib/decoder.f90
+++ lib/decoder.f90
@@ -7,7 +7,7 @@ subroutine decoder(ntrSeconds,ndepth,nRxLog,c00)
   parameter (NSMAX=22000)            !Max length of saved spectra
   character*22 msg
   character*33 line
-  character*80 fmt
+  character*80 fmt,fmt14
   real*4 ccfred(NSMAX)
   integer*1 i1SoftSymbols(207)
   integer*2 id2
@@ -22,7 +22,7 @@ subroutine decoder(ntrSeconds,ndepth,nRxLog,c00)
   save
 
   if(first) then
-     limtrace=0
+     limtrace=1000000
      lu=12
      open(12,file='timer.out',status='unknown')
      open(14,file='wsjtx_rx.log',status='unknown',position='append')
@@ -42,31 +42,36 @@ subroutine decoder(ntrSeconds,ndepth,nRxLog,c00)
   newdat=1
   nsynced=0
   ndecoded=0
-  limit=1000
-  if(ndepth.ge.2) limit=20000
-  if(ndepth.ge.3) limit=100000
+  limit=200
+  if(ndepth.ge.2) limit=2000
+  if(ndepth.ge.3) limit=20000
 
   nsps=0
   if(ntrMinutes.eq.1) then
      nsps=6912
      df3=1500.0/2048.0
-     fmt='(i4.4,i4,i5,f6.1,f8.0,f6.1,3x,a22,5x,i6)'
+     fmt='(i4.4,i4,i5,f6.1,f8.0,f6.1,3x,a22)'
+     fmt14='(i4.4,i4,i5,f6.1,f8.0,f6.1,i8,3x,a22)'
   else if(ntrMinutes.eq.2) then
      nsps=15360
      df3=1500.0/2048.0
-     fmt='(i4.4,i4,i5,f6.1,f8.1,f6.2,3x,a22,5x,i6)'
+     fmt='(i4.4,i4,i5,f6.1,f8.1,f6.2,3x,a22)'
+     fmt14='(i4.4,i4,i5,f6.1,f8.1,f6.2,i8,3x,a22)'
   else if(ntrMinutes.eq.5) then
      nsps=40960
      df3=1500.0/6144.0
-     fmt='(i4.4,i4,i5,f6.1,f8.1,f6.2,3x,a22,5x,i6)'
-  else if(ntrMinutes.eq.10) then
+     fmt='(i4.4,i4,i5,f6.1,f8.1,f6.2,3x,a22)' 
+     fmt14='(i4.4,i4,i5,f6.1,f8.1,f6.2,i8,3x,a22)'
+ else if(ntrMinutes.eq.10) then
      nsps=82944
      df3=1500.0/12288.0
-     fmt='(i4.4,i4,i5,f6.1,f8.2,f6.2,3x,a22,5x,i6)'
+     fmt='(i4.4,i4,i5,f6.1,f8.2,f6.2,3x,a22)'
+     fmt14='(i4.4,i4,i5,f6.1,f8.2,f6.2,i8,3x,a22)'
   else if(ntrMinutes.eq.30) then
      nsps=252000
      df3=1500.0/32768.0
-     fmt='(i4.4,i4,i5,f6.1,f8.2,f6.2,3x,a22,5x,i6)'
+     fmt='(i4.4,i4,i5,f6.1,f8.2,f6.2,3x,a22)'
+     fmt14='(i4.4,i4,i5,f6.1,f8.2,f6.2,i8,3x,a22)'
   endif
   if(nsps.eq.0) stop 'Error: bad TRperiod'    !Better: return an error code###
 
@@ -84,6 +89,7 @@ subroutine decoder(ntrSeconds,ndepth,nRxLog,c00)
 ! Write date and time to lu 14     
   endif
 
+  nRxLog=0
   fgood=0.
   df8=1500.0/(nsps/8)
   sbest=0.
@@ -101,6 +107,7 @@ subroutine decoder(ntrSeconds,ndepth,nRxLog,c00)
      call timer('decode9 ',0)
      call decode9(i1SoftSymbols,limit,nlim,msg)
      call timer('decode9 ',1)
+ 
      sync=ccfred(i) - 2.0
      if(sync.lt.0.0) sync=0.0
      nsync=sync
@@ -116,7 +123,7 @@ subroutine decoder(ntrSeconds,ndepth,nRxLog,c00)
 
      if(msg.ne.'                      ') then
         write(13,fmt) nutc,nsync,nsnr,xdt,1000.0+fpk,drift,msg
-        write(14,fmt) nutc,nsync,nsnr,xdt,1000.0+fpk,drift,msg,nlim
+        write(14,fmt14) nutc,nsync,nsnr,xdt,1000.0+fpk,drift,nlim,msg
         fgood=f
         nsynced=1
         ndecoded=1
@@ -125,7 +132,7 @@ subroutine decoder(ntrSeconds,ndepth,nRxLog,c00)
   i1=max(ia,i-10)
   i2=min(ib,i+10)
   ccfred(i1:i2)=0.
-  if(maxval(ccfred(ia:ib)).gt.3.0) go to 10
+   if(maxval(ccfred(ia:ib)).gt.3.0) go to 10
 
   if(fgood.eq.0.0) then
      write(13,1020) line
