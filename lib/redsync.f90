e30bc0278491f5606793a6064595ae7bd0ff4b5a
Joe Taylor <k1jt@arrl.org>
2012-11-15 23:40:57 +0000

    Add nlim to decoded text lines in wsjtx_rx.log. Increase amplitude of red curve.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@2736 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- lib/redsync.f90
+++ lib/redsync.f90
@@ -31,7 +31,7 @@ subroutine redsync(ss,ntrperiod,ihsym,iz,red)
   smax=0.
   do i=1,iz
 !     red(i)=0.3*db(red(i))
-     red(i)=sqrt(red(i))
+     red(i)=2.0*sqrt(red(i))
      smax=max(smax,red(i))
   enddo
   h=10.
