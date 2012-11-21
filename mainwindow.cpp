ff472c83d3b3c796d3f62231f7704b5b6692e8ca
Joe Taylor <k1jt@arrl.org>
2012-11-21 17:49:16 +0000

    Bump version number to 0.5.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@2758 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,7 +18,7 @@ WideGraph* g_pWideGraph = NULL;
 QSharedMemory mem_jt9("mem_jt9");
 
 QString rev="$Rev$";
-QString Program_Title_Version="  WSJT-X   v0.4, r" + rev.mid(6,4) +
+QString Program_Title_Version="  WSJT-X   v0.5, r" + rev.mid(6,4) +
                               "    by K1JT";
 
 //-------------------------------------------------- MainWindow constructor
