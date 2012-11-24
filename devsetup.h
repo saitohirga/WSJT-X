abe540754b45f7fc231d48f2132e21e7eb84c74b
Joe Taylor <k1jt@arrl.org>
2012-11-24 16:30:39 +0000

    Use correct upper/lower case for MyCall, MyGrid.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@2770 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- devsetup.h
+++ devsetup.h
@@ -33,6 +33,11 @@ public:
 public slots:
   void accept();
 
+private slots:
+  void on_myCallEntry_editingFinished();
+
+  void on_myGridEntry_editingFinished();
+
 private:
   Ui::DialogSndCard ui;
 };
