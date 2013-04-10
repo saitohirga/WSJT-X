119b2ed43bf218f4ce7d181a3e528cd72c927b32
Joe Taylor <k1jt@arrl.org>
2013-04-10 14:41:16 +0000

    1. Send values of fMin, fMax, to the jt9 decoder. 2. Option for user to set default fonts. 3. Minor tweaks to GUI appearance.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@3150 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- main.cpp
+++ main.cpp
@@ -4,8 +4,21 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+  QApplication a(argc, argv);
+
+  QFile f("fonts.txt");
+  if(f.open(QIODevice::ReadOnly)) {
+     QTextStream in(&f);                          //Example:
+     QString fontFamily;                          // helvetica
+     qint32 fontSize,fontWeight;                  // 8,50
+     in >> fontFamily >> fontSize >> fontWeight;
+     f.close();
+     QFont font;
+     font=QFont(fontFamily,fontSize,fontWeight);
+     a.setFont(font);
+  }
+
+  MainWindow w;
+  w.show();
+  return a.exec();
 }
