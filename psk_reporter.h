588d329454aee810e7a67026c26cb7ee603784f4
Edson W. R. Pereira <ewpereira@gmail.com>
2013-04-16 19:22:53 +0000

    Added class for PSK Reporter. Still need to wire class to mainwindow.cpp
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@3182 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- psk_reporter.h
+++ psk_reporter.h
@@ -1,18 +1,41 @@
 #ifndef PSK_REPORTER_H
 #define PSK_REPORTER_H
 
-#include <QObject>
+#include <QtCore>
+#include <QUdpSocket>
+#include <QHostInfo>
 
 class PSK_Reporter : public QObject
 {
     Q_OBJECT
 public:
     explicit PSK_Reporter(QObject *parent = 0);
+    void setLocalStation(QString call, QString grid, QString programInfo);
+    void addRemoteStation(QString call, QString grid, QString freq, QString mode, QString snr, QString time);
     
 signals:
     
 public slots:
-    
+    void sendReport();
+
+private:
+    QString m_header_h;
+    QString m_rxInfoDescriptor_h;
+    QString m_txInfoDescriptor_h;
+    QString m_randomId_h;
+    QString m_linkId_h;
+
+    QString m_rxCall;
+    QString m_rxGrid;
+    QString m_progId;
+
+    QQueue< QHash<QString,QString> > m_spotQueue;
+
+    QUdpSocket *m_udpSocket;
+
+    QTimer *reportTimer;
+
+    int m_sequenceNumber;
 };
 
 #endif // PSK_REPORTER_H
