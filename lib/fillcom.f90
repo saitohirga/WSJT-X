80ba5dcea3cab69a20a657b8ddbb7038977d9f10
Joe Taylor <k1jt@arrl.org>
2013-04-16 16:38:25 +0000

    Add fillcom.f90
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@3178 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

---
+++ lib/fillcom.f90
@@ -0,0 +1,25 @@
+subroutine fillcom(nutc0)
+  character*20 datetime
+  common/npar/nutc,ndiskdat,ntrperiod,nfqso,newdat,npts8,nfa,nfb,ntol,  &
+       kin,nzhsym,nsave,nagain,ndepth,nrxlog,nfsample,datetime
+  save
+
+  nutc=nutc0
+  ndiskdat=1
+  ntrperiod=60
+  nfqso=1500
+  newdat=1
+  npts8=74736
+  nfa=1000
+  nfb=2000
+  ntol=10
+  kin=1024
+  nzhsym=173
+  nsave=0
+  ndepth=3
+  nrxlog=1
+  nfsample=12000
+  datetime="2013-Apr-16 15:13"
+  
+  return
+end subroutine fillcom
