346e04cd821b7b1145b0c2ca52e7262f96ab2b3f
Joe Taylor <k1jt@arrl.org>
2013-05-08 15:23:02 +0000

    1. Add report to wsjt_status.txt, for JT-Alert. 2. Fix annoying bug when trying to open rig using a non-existent or    busy serial port. 3. Add acknowledgments to AE4JY 4. Tentative tweaks to "Tab 2" GUI layout.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@3261 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- about.cpp
+++ about.cpp
@@ -10,9 +10,9 @@ CAboutDlg::CAboutDlg(QWidget *parent, QString Revision) :
   ui->labelTxt->clear();
   m_Str  = "<html><h2>" + m_Revision + "</h2>\n\n";
   m_Str += "WSJT-X implements experimental mode JT9 for <br>";
-  m_Str += "Amateur Radio communication at MF and LF.  <br><br>";
+  m_Str += "Amateur Radio communication at HF, MF, and LF.  <br><br>";
   m_Str += "Copyright 2001-2013 by Joe Taylor, K1JT.   Additional <br>";
-  m_Str += "contributions from PY2SDR, VK4BDJ, and AC6SL. <br>";
+  m_Str += "contributions from AE4JY, PY2SDR, VK4BDJ, and AC6SL. <br>";
   ui->labelTxt->setText(m_Str);
 }
 
