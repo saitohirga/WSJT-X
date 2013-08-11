e2d1898a3fc067febb1f34a483b36f73ecaacb0c
Murray Curtis <vk3acf@littlewoodmansion.com>
2013-08-11 11:45:17 +0000

    1. Moved font width calc for DXCC enitity display from LogBook to DisplayText. 2. The available character count is now updated by catching resizeEvents
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@3545 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- displaytext.h
+++ displaytext.h
@@ -7,15 +7,25 @@ class DisplayText : public QTextBrowser
 {
     Q_OBJECT
 public:
-    explicit DisplayText(QWidget *parent = 0);
+  explicit DisplayText(QWidget *parent = 0);
+
+  void setFont(QFont font);
+  int getMaxDisplayedCharacters() { return _maxDisplayedCharacters; }
+
 
 signals:
   void selectCallsign(bool shift, bool ctrl);
 
 public slots:
 
+
 protected:
   void mouseDoubleClickEvent(QMouseEvent *e);
+  void resizeEvent(QResizeEvent * event);
+
+private:
+  int _fontWidth;
+  int _maxDisplayedCharacters;
 
 };
 
