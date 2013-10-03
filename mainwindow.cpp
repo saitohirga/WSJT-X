756f256321f5cefdda6abe6ceb2146982fac8888
Joe Taylor <k1jt@arrl.org>
2013-10-03 14:07:39 +0000

    Bump the version number to 1.2.1, preparing for new beta release.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@3587 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -36,7 +36,7 @@ QTextEdit* pShortcuts;
 QTcpSocket* commanderSocket = new QTcpSocket(0);
 
 QString rev="$Rev$";
-QString Program_Title_Version="  WSJT-X   v1.2, r" + rev.mid(6,4) +
+QString Program_Title_Version="  WSJT-X   v1.2.1, r" + rev.mid(6,4) +
                               "    by K1JT";
 
 //-------------------------------------------------- MainWindow constructor
