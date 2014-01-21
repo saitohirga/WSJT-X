5bb262246da556685e7e4e8b5ca3bd64a5a67264
Joe Taylor <k1jt@arrl.org>
2014-01-21 21:24:56 +0000

    First commit of the KI7MT documentation directory and contents.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@3655 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

---
+++ doc/build-doc.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+#
+# Part of the wsjtx-doc project
+# Builds all *.txt files found in $(PWD)/source
+
+# exit on any error
+set -e
+
+# set script path's
+base_dir=$(pwd)
+src_dir="$base_dir/source"
+style_dir="$base_dir/style"
+log_dir="$base_dir/logs"
+
+# style sheet selection
+main_style=asciidoc.css
+toc2_style=toc2.css
+
+# This is temporary. Final version will loop through a directory of files
+c_asciidoc="asciidoc -b xhtml11 -a max-width=1024px"
+clear
+echo Building Main Page HTML
+echo .. Main Page Without TOC
+$c_asciidoc -o wsjtx-main.html ${src_dir}/wsjtx-main.txt
+echo .. Done
+
+echo .. Main Page With TOC
+$c_asciidoc -a toc -o wsjtx-main-toc.html ${src_dir}/wsjtx-main.txt
+echo .. Done
+
+echo .. Main Page With TOC2
+$c_asciidoc -a toc2 -o wsjtx-main-toc2.html ${src_dir}/wsjtx-main.txt
+echo .. Done
+
+echo Building Rig Configuration Sheets
+echo Building Yaesu
+$c_asciidoc -o yaesu.html ${src_dir}/yaesu.txt
+echo .. Done
+
+echo Building regtemplate
+$c_asciidoc -o rigtemplate.html ${src_dir}/rigtemplate.txt
+echo Done
+echo
+echo All HTML docs have been saved to "$base_dir"
+
+exit 0
+
+
