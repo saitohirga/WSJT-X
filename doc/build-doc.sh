44543d385dfa50882061d9f3ada906abf4068707
Greg Beam <ki7mt@yahoo.com>
2014-01-25 22:26:21 +0000

    User Guide Docs Update
    * Moved doc/source/*.txt to AsciiDoc ext source/*.adoc
    - Correctly identifies AsciiDoc files
    
    * wsjtx-main.adoc
    - Updated links and include:: for *.adoc name change
    
    * Added and updated rig-config-* files.
    * Added inital draft of quick-reference.adoc
    
    * build-doc.sh
    - Updated the script to build new files and .adoc name change
    
    * Removed:
    - yaesu.txt rigtemplate.txt rig-configuration.txt
    
    
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@3662 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- doc/build-doc.sh
+++ doc/build-doc.sh
@@ -3,8 +3,8 @@
 # Description     : WSJT-X Documentation build script
 # Author          : KI7MT
 # Email           : ki7mt@yahoo.com
-# Date            : JAN-21-2014
-# Version         : 0.2
+# Date            : JAN-24-2014
+# Version         : 0.3
 # Usage           : ./build-doc.sh [ option ]
 # Notes           : requires asciidoc, source-highlight
 #==============================================================================
@@ -26,60 +26,86 @@ c_asciidoc="asciidoc -b xhtml11 -a max-width=1024px"
 script_name=$(basename $0)
 doc_version="1.2.2"
 
-
 # build functions
 function build_no_toc() { # no toc
-    echo -e ${yellow}'Building Without TOC'${no_col}
-    $c_asciidoc -o wsjtx-main.html $src_dir/wsjtx-main.txt
-    echo -e ${green}'. Finished wsjtx-main.html'${no_col}
+    echo -e ${yellow}'Building Main With No TOC'${no_col}
+    $c_asciidoc -o wsjtx-main.html $src_dir/wsjtx-main.adoc
+    echo -e ${green}'.. wsjtx-main.html'${no_col}
 }
 
 function build_toc1() { # top toc
-    echo -e ${yellow}'Building with Top TOC'${no_col}
-    $c_asciidoc -a toc -o wsjtx-main-toc1.html $src_dir/wsjtx-main.txt
-    echo -e ${green}'. Finished wsjtx-main-toc1.html'${no_col}
+    echo -e ${yellow}'Building Main with Top TOC'${no_col}
+    $c_asciidoc -a toc -o wsjtx-main-toc1.html $src_dir/wsjtx-main.adoc
+    echo -e ${green}'.. wsjtx-main-toc1.html'${no_col}
 }
 
 function build_toc2() { # left toc
-    echo -e ${yellow}'Building with Left TOC'${no_col}
-    $c_asciidoc -a toc2 -o wsjtx-main-toc2.html $src_dir/wsjtx-main.txt
-    echo -e ${green}'. Finished wsjtx-main-toc2.html'${no_col}
+    echo -e ${yellow}'Building Main with Left TOC'${no_col}
+    $c_asciidoc -a toc2 -o wsjtx-main-toc2.html $src_dir/wsjtx-main.adoc
+    echo -e ${green}'.. wsjtx-main-toc2.html'${no_col}
+}
+
+function build_support_pages() { # build all remaining pages
+  echo
+  echo -e ${yellow}'Building Support Pages'${no_col}
+  $c_asciidoc -o rig-config-main.html $src_dir/rig-config-main.adoc
+  echo -e ${green}'.. rig-config-main.html'${no_col}
+
+  # setup rig file array
+  declare -a subpage=('adat' 'alinco' 'aor' 'drake' 'electro' 'flexrad' 'icom' \
+'kenwood' 'softrock' 'tentec' 'yaesu')
+  
+  # loop through rig-config pages
+  for rig in "${subpage[@]}"
+  do
+    $c_asciidoc -a toc2 -o rig-config-$rig.html $src_dir/rig-config-$rig.adoc
+    echo -e ${green}".. rig-config-$rig.html"${no_col}
+  done
+
+  $c_asciidoc -o rig-config-template.html $src_dir/rig-config-template.adoc
+  echo -e ${green}'.. rig-config-template.html'${no_col}
+
+  $c_asciidoc -a toc2 -o quick-reference.html $src_dir/quick-reference.adoc
+  echo -e ${green}'.. quick-reference.html'${no_col}
 }
 
 # start the main script
 clear
-# Hard coded version info to build outside of source tree.
-echo -e ${yellow}"*** Building WSJT-X User Guide for:" ${cyan}$doc_version${no_col}${yellow}" ***\n" ${no_col}
+echo -e ${yellow}"*** Building WSJT-X User Guide for:" ${cyan}$doc_version\
+${no_col}${yellow}" ***\n" ${no_col}
 
 # without TOC
 if [[ $1 = "" ]]
   then
     build_no_toc
+    build_support_pages
 
 # top TOC
 elif [[ $1 = "toc1" ]]
   then
     build_toc1
+    build_support_pages
 
 # left TOC
 elif [[ $1 = "toc2" ]]
   then
   build_toc2
+  build_support_pages
 
 # all toc versions
 elif [[ $1 = "all" ]]
   then
-    echo -e ${yellow}'Building all TOC versions'${no_col}
-    echo
     build_no_toc
-    echo
     build_toc1
-    echo
     build_toc2
+    build_support_pages
 
-# if something other than "", toc1, toc2 or all is entered as $1 display usage
-# message and exit. this should be re-written to redirect the user to select
-# 1 of 4 proper options v.s. exiting.
+# Usage: if something other than "", toc1, toc2 or all is entered as $1 display usage
+# message and exit.
+#
+# To-Do: this should be re-written to redirect the user to select
+# 1 of 4 proper options v.s. exiting. Future version should provide
+# a terminal GUI, Whiptail, Dialog, Zenity etc.
 else
     clear
     echo -e ${red}" * INPUT ERROR *\n"${no_col}
@@ -94,22 +120,9 @@ else
     echo
     exit 1
 fi
-
-# build the rig template page
-  echo
-  echo -e ${yellow}'Building Rig Template'${no_col}
-  $c_asciidoc -o rigtemplate.html $src_dir/rigtemplate.txt
-  echo -e ${green}'. Finished'${no_col}
- 
-# build the rig sheets
-# this section will should be modified to loop through rig template(s) either by
-# name or directory ./rig/rig_* could be used.
-  echo
-  echo -e ${yellow}'Building Rig Configuration Sheets'${no_col}
-  $c_asciidoc -o yaesu.html $src_dir/yaesu.txt
-  echo -e ${green}'. Finished'${no_col}
   echo
   echo -e ${yellow}'All HTML files have been saved to:'${no_col}${cyan} "$base_dir" ${no_col}
   echo
+
 exit 0
 
