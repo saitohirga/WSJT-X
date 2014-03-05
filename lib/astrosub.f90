294068ff5bc087a8f77c21a6cd1e7053a5c4ef1a
Joe Taylor <k1jt@arrl.org>
2014-03-05 20:06:44 +0000

    Anoter missing file
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@3837 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

---
+++ lib/astrosub.f90
@@ -0,0 +1,14 @@
+subroutine astrosub(nyear,month,nday,uth8,nfreq,mygrid,hisgrid,          &
+     AzSun8,ElSun8,AzMoon8,ElMoon8,AzMoonB8,ElMoonB8,ntsky,ndop,ndop00,  &
+     RAMoon8,DecMoon8,Dgrd8,poloffset8,xnr8,techo8)
+
+  implicit real*8 (a-h,o-z)
+  character*6 mygrid,hisgrid
+
+  call astro0(nyear,month,nday,uth8,nfreq,mygrid,hisgrid,                &
+     AzSun8,ElSun8,AzMoon8,ElMoon8,AzMoonB8,ElMoonB8,ntsky,ndop,ndop00,  &
+     dbMoon8,RAMoon8,DecMoon8,HA8,Dgrd8,sd8,poloffset8,xnr8,dfdt,dfdt0,  &
+     width1,width2,w501,w502,xlst8,techo8)
+
+  return  
+end subroutine astrosub
