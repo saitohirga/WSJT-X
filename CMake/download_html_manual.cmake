7fad4590364a51e2e114aebbfd99cf35384a9127
Bill Somerville <g4wjs@classdesign.com>
2014-09-24 23:46:32 +0000

    Add local HTML help file to help menu.
    So that a manual can be viewed without an Internet connection a second
    manual action has been added to  the WSJT_X help menu. This shells out
    to the default web browser with a local file target.
    
    The build has been enhanced to install and package the current version
    of the  HTML user manual  as downloaded from  the project web  site at
    build time
    
    The package  file locations have  been adjusted to be  compatible with
    the QStandardPaths  class so that  its locate()  method can be  used to
    portably access resources like this local help file
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@4359 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

---
+++ CMake/download_html_manual.cmake
@@ -0,0 +1,16 @@
+#
+# CMake script to fetch the current HTML manual
+#
+message (STATUS "file: ${URL}/${NAME}")
+file (
+  DOWNLOAD ${URL}/${NAME} contrib/${NAME}
+  TIMEOUT 10
+  STATUS manual_STATUS
+  LOG manual_LOG
+  SHOW_PROGRESS
+  )
+list (GET manual_STATUS 0 manual_RC)
+if (manual_RC)
+  message (WARNING "${manual_STATUS}")
+  message (FATAL_ERROR "${manual_LOG}")
+endif (manual_RC)
