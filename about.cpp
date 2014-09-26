e2ec2ed750f6fcbbd12bb84c13922ecf7bb0eb03
Joe Taylor <k1jt@arrl.org>
2014-09-26 12:19:17 +0000

    Merging change from r4376 in branches/wsjtx-1.4 into branches/wsjtx.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@4380 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- about.cpp
+++ about.cpp
@@ -16,7 +16,7 @@ CAboutDlg::CAboutDlg(QString const& program_title, QWidget *parent) :
                          "&copy; 2001-2014 by Joe Taylor, K1JT, with grateful <br>"
                          "acknowledgment for contributions from AC6SL, AE4JY, <br>"
                          "DJ0OT, G4KLA, G4WJS, K3WYC, KA6MAL, KA9Q, KB1ZMX, <br>"
-                         "KK1D, PY2SDR, VK3ACF, VK4BDJ, W4TI, W4TV, and W9MDB.<br>");
+                         "KI7MT, KK1D, PY2SDR, VK3ACF, VK4BDJ, W4TI, W4TV, and W9MDB.<br>");
 }
 
 CAboutDlg::~CAboutDlg()
