c40cec50de74a04b7caf5b46b44596ead651424f
Bill Somerville <g4wjs@classdesign.com>
2014-10-04 11:47:03 +0000

    Menu action to open the log directory
    Since the log files have moved to  a hidden directory a menu action to
    open the logs directory in a suitable system applicaiton is needed.
    
    Added "Open log directory" to File manu bar pop up menu.
    
    Merged from wsjtx-1.4 branch.
    
    
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@4471 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- mainwindow.h
+++ mainwindow.h
@@ -103,6 +103,7 @@ private slots:
   void on_actionOpen_next_in_directory_triggered();
   void on_actionDecode_remaining_files_in_directory_triggered();
   void on_actionDelete_all_wav_files_in_SaveDir_triggered();
+  void on_actionOpen_log_directory_triggered ();
   void on_actionNone_triggered();
   void on_actionSave_all_triggered();
   void on_actionKeyboard_shortcuts_triggered();
