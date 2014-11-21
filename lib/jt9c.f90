7223a29788574d77a080ea8745bc114ca5e0f5b1
Joe Taylor <k1jt@arrl.org>
2014-11-21 01:40:24 +0000

    Decode from disk sets npatience = 2.
    This means that the first decode from a saved data file will be slow,
    but the saved wisdom for the decoded mode(s) will be better than
    for the default npatience = 1.  All subsequent decodes in the same 
    mode(s) will take advantage of the newly saved wisdom.
    
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@4616 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- lib/jt9c.f90
+++ lib/jt9c.f90
@@ -8,10 +8,12 @@ subroutine jt9c(ss,savg,id2,nparams0)
   character*20 datetime
   common/npar/nutc,ndiskdat,ntrperiod,nfqso,newdat,npts8,nfa,nfsplit,nfb,   &
        ntol,kin,nzhsym,nsave,nagain,ndepth,ntxmode,nmode,datetime
+  common/patience/npatience
   equivalence (nparams,nutc)
 
   nutc=id2(1)+int(savg(1))             !Silence compiler warning
   nparams=nparams0                     !Copy parameters into common/npar/
+  if(ndiskdat.ne.0) npatience=2
 
   call flush(6)
 !  if(sum(nparams).ne.0) call decoder(ss,id2,ldir)
