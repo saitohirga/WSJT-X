953c55fa41f28acb95747594ac7ea98f01d2a8a6
Bill Somerville <g4wjs@classdesign.com>
2014-11-27 17:15:26 +0000

    Add on-line KVASD install to Windows NSIS installer
    To comply  with the  WSJT_X GPL  v3 license,  the closed  source KVASD
    executable  must  be  delivered   separately  and  optionally  to  end
    users. To facilitate  this a new install option has  been added to the
    Windows  installer  to install  KVASD  by  downloading it  at  install
    time. The installer  first downloads the KVASD EULA  from the Internet
    and presents it  to the user for reading and  agreement.  If agreement
    is  given  the  KVASD  binary  is downloaded  from  the  Internet  and
    checksummed using an  MD5 hash, if that passes, it  is installed along
    with WSJT-X.  An  uninstall record is also written to  ensure that, if
    installed, KVASD will be un-installed if WSJT-X is un-installed.
    
    To facilitate this enhancment to the NSIS installer the standard CPack
    NSIS  installer script  template has  been overridden  with a  project
    specific version.   This version also  implements the more  modern and
    recomended nsDialogs mechanism for  installer custom page construction
    including the install options page. See CMake/Modules/NSIS.template.in
    for  details   and  CMakeCPackOptions.cmake.in   for  usage   in  this
    installer.
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx-1.4@4662 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- CMake/download_kvasd.cmake
+++ CMake/download_kvasd.cmake
@@ -1,25 +1,34 @@
 #
 # CMake script to fetch kvasd binary for the current platform
 #
-set (kvasd_NAME https://svn.code.sf.net/p/wsjt/wsjt/trunk/kvasd-binary/${SYSTEM_NAME}/kvasd${EXECUTABLE_SUFFIX})
+set (kvasd_NAME ${URL}/${SYSTEM_NAME}/kvasd${EXECUTABLE_SUFFIX})
 message (STATUS "file: ${kvasd_NAME}")
-if (APPLE)
-  set (__kvasd_md5sum 198dbdde1e4b04f9940f63731097ee35)
-elseif (WIN32)
-  set (__kvasd_md5sum 7b16809e51126a01bd02aed44427510c)
-elseif (UNIX)
-  set (__kvasd_md5sum 28a6f8ba2efc3df02af2a781bd3ab654)
-endif ()
+
+file (
+  DOWNLOAD ${kvasd_NAME}.md5 contrib/kvasd${EXECUTABLE_SUFFIX}.md5
+  TIMEOUT 120
+  STATUS status
+  LOG log
+  SHOW_PROGRESS
+  )
+list (GET staus 0 rc)
+if (rc)
+  message (WARNING "${status}")
+  message (FATAL_ERROR "${log}")
+endif (rc)
+file (READ contrib/kvasd${EXECUTABLE_SUFFIX}.md5 md5sum)
+string (REGEX MATCH "[0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f]" md5sum ${md5sum})
+
 file (
   DOWNLOAD ${kvasd_NAME} contrib/kvasd${EXECUTABLE_SUFFIX}
   TIMEOUT 120
-  STATUS kvasd_STATUS
-  LOG kvasd_LOG
+  STATUS status
+  LOG log
   SHOW_PROGRESS
-  EXPECTED_MD5 ${__kvasd_md5sum}
+  EXPECTED_MD5 ${md5sum}
   )
-list (GET kvasd_STATUS 0 kvasd_RC)
-if (kvasd_RC)
-  message (WARNING "${kvasd_STATUS}")
-  message (FATAL_ERROR "${kvasd_LOG}")
-endif (kvasd_RC)
+list (GET status 0 rc)
+if (rc)
+  message (WARNING "${status}")
+  message (FATAL_ERROR "${log}")
+endif (rc)
