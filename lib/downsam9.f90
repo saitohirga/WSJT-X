8845f63792b92709ad4dbe9469a310fb35df6b1b
Bill Somerville <g4wjs@classdesign.com>
2015-02-08 09:53:20 +0000

    Remove redundant copy of large array in JT9 decoder
    Also moved  the same large array  from stack to heap  which along with
    other prior  changes now allows  the Windows jt9 OpenMP  executable to
    run with a default stack size again.
    
    This also removes a crash on the Mac version which was probably due to
    excessive stack usage.
    
    Net result is an even faster JT9 decoder.
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@4942 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- lib/downsam9.f90
+++ lib/downsam9.f90
@@ -10,26 +10,18 @@ subroutine downsam9(id2,npts8,nsps8,newdat,nspsd,fpk,c2,nz2)
   parameter (NMAX1=604800)
   type(C_PTR) :: plan                        !Pointers plan for big FFT
   integer*2 id2(0:8*npts8-1)
-  real*4 x1(0:NMAX1-1)
+  real*4, pointer :: x1(:)
   complex c1(0:NMAX1/2)
   complex c2(0:1440-1)
   real s(5000)
   logical first
   common/patience/npatience,nthreads
   data first/.true./
-  save plan,first,c1,s
+  save plan,first,c1,s,x1
 
   nfft1=NMAX1                                !Forward FFT length
   df1=12000.0/nfft1
   npts=8*npts8
-
-  if(newdat.eq.1) then
-     fac=6.963e-6                            !Why this weird constant?
-     do i=0,npts-1
-        x1(i)=fac*id2(i)
-     enddo
-     x1(npts:nfft1-1)=0.                      !Zero the rest of x1
-  endif
 
   if(first) then
      nflags=FFTW_ESTIMATE
@@ -40,6 +32,9 @@ subroutine downsam9(id2,npts8,nsps8,newdat,nspsd,fpk,c2,nz2)
 ! Plan the FFTs just once
 
      !$omp critical(fftw) ! serialize non thread-safe FFTW3 calls
+     plan=fftwf_alloc_real(NMAX1)
+     call c_f_pointer(plan,x1,[NMAX1])
+     x1(0:NMAX1-1) => x1        !remap bounds
      call fftwf_plan_with_nthreads(nthreads)
      plan=fftwf_plan_dft_r2c_1d(nfft1,x1,c1,nflags)
      call fftwf_plan_with_nthreads(1)
@@ -50,9 +45,7 @@ subroutine downsam9(id2,npts8,nsps8,newdat,nspsd,fpk,c2,nz2)
 
   if(newdat.eq.1) then
      fac=6.963e-6                             !Why this weird constant?
-     do i=0,npts-1
-        x1(i)=fac*id2(i)
-     enddo
+     x1(0:npts-1)=fac*id2(0:npts-1)
      x1(npts:nfft1-1)=0.                      !Zero the rest of x1
      call timer('FFTbig9 ',0)
      call fftwf_execute_dft_r2c(plan,x1,c1)
