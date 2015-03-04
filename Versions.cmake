127a60be68339fd256595f99a6841ea317f50884
Bill Somerville <g4wjs@classdesign.com>
2015-03-04 13:21:41 +0000

    Prep for v1.4.0-rc4
    Merged from wsjtx-1.4 branch.
    
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@5002 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- Versions.cmake
+++ Versions.cmake
@@ -4,7 +4,3 @@ set (WSJTX_VERSION_MINOR 5)
 set (WSJTX_VERSION_PATCH 0)
 set (WSJTX_RC 0)		 # release candidate number, comment out or zero for development versions
 set (WSJTX_VERSION_IS_RELEASE 0) # set to 1 for final release build
-
-set (CONFIG_TEST_VERSION_MAJOR 0)
-set (CONFIG_TEST_VERSION_MINOR 2)
-set (CONFIG_TEST_VERSION_PATCH 13)
