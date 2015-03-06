7f4d0723ed8c2c001a1062c32987a21af71116ee
Bill Somerville <g4wjs@classdesign.com>
2015-03-06 17:18:24 +0000

    Correct the date in Copyright notice.
    Merged from wsjtx branch.
    
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx-1.4@5013 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- about.cpp
+++ about.cpp
@@ -21,7 +21,7 @@ CAboutDlg::CAboutDlg(QWidget *parent) :
                          + "</h2>\n\n"
                          "WSJT-X implements digital modes JT9 and JT65 for <br>"
                          "Amateur Radio communication.  <br><br>"
-                         "&copy; 2001-2014 by Joe Taylor, K1JT, with grateful <br>"
+                         "&copy; 2001-2015 by Joe Taylor, K1JT, with grateful <br>"
                          "acknowledgment for contributions from AC6SL, AE4JY, <br>"
                          "DJ0OT, G4KLA, G4WJS, K3WYC, KA6MAL, KA9Q, KB1ZMX, <br>"
                          "KI7MT, KK1D, PY2SDR, VK3ACF, VK4BDJ, W4TI, W4TV, and W9MDB.<br>");
