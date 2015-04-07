fe70882ef27d095fcadfa4089bede9701d9dca02
Bill Somerville <g4wjs@classdesign.com>
2015-04-07 13:12:23 +0000

    Remove unneeded debug message
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@5180 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- Configuration.cpp
+++ Configuration.cpp
@@ -2359,7 +2359,6 @@ void Configuration::impl::update_audio_channels (QComboBox const * source_combo_
 void Configuration::impl::set_application_font (QFont const& font)
 {
   qApp->setStyleSheet (qApp->styleSheet () + "* {" + font_as_stylesheet (font) + '}');
-  qDebug () << "app style sheet:" << qApp->styleSheet ();
 }
 
 // load all the supported rig names into the selection combo box
