03192a2fcc347662ffd1ca16dc0b2eda466e5dbe
Bill Somerville <g4wjs@classdesign.com>
2015-04-26 16:50:40 +0000

    Some HRD commands need a settling time otherwise they don't stick
    This seems  to be  limited to  the Yaesu  FTdx3000 "RX  A" and  "RX B"
    commands so a short delay has been added after these sort of commands.
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@5299 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- DXLabSuiteCommanderTransceiver.hpp
+++ DXLabSuiteCommanderTransceiver.hpp
@@ -33,7 +33,7 @@ public:
 protected:
   void do_start () override;
   void do_stop () override;
-  void do_frequency (Frequency, MODE = UNK) override;
+  void do_frequency (Frequency, MODE) override;
   void do_tx_frequency (Frequency, bool rationalise_mode) override;
   void do_mode (MODE, bool rationalise) override;
   void do_ptt (bool on) override;
