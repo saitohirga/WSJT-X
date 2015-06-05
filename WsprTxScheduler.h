fbd3058d00e088eaa1b82c8553cb29b7a138facf
Steven Franke <s.j.franke@icloud.com>
2015-06-05 19:48:53 +0000

    Fix up WsprTxScheduler.h, place variables in unnamed namespace, etc.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@5540 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- WsprTxScheduler.h
+++ WsprTxScheduler.h
@@ -1,19 +1,6 @@
-#include <cstdio>
-#include <cstdlib>
-#include <ctime>
-#include <cstring>
-#include <iostream>
+#ifndef WSPR_TX_SCHEDULER_H_
+#define WSPR_TX_SCHEDULER_H_
 
-using namespace std;
+int next_tx_state (int pctx);
 
-char tx[6][10];
-int tx_table_2hr_slot=-1, tx_table_pctx=0;
-
-int tx_band_sum(char bsum[10]);
-int tx_add_to_band(int band);
-int tx_sum();
-int tx_add_one(char* tx);
-int tx_trim(char* tx, int ntxlim);
-void tx_print();
-int create_tx_schedule(int pctx);
-int next_tx_state(int pctx);
+#endif
