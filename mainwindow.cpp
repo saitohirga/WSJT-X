f12de9e9e63a5f3862ce17e5f2dd686733bb7ab5
Steven Franke <s.j.franke@icloud.com>
2015-06-05 15:36:29 +0000

    Move call to tx scheduler per Bill's instructions.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@5539 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4296,8 +4296,7 @@ void MainWindow::bandHopping()
             << "tune:" << hop_data.tune_required_
             << "tx:" << hop_data.tx_next_;
 
-//  if (m_auto &&hop_data.tx_next_) {
-  if ( m_auto && next_tx_state(m_pctx) ) {
+  if (m_auto &&hop_data.tx_next_) {
     m_nrx = 0;
   } else {
     m_nrx = 1;
