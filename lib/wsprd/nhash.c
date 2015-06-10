9adb558c95b9e54f942dc59e414f19d4bd04b61e
Joe Taylor <k1jt@arrl.org>
2015-06-10 20:23:18 +0000

    Must include nhash.h *after* the system includes.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@5577 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- lib/wsprd/nhash.c
+++ lib/wsprd/nhash.c
@@ -70,10 +70,9 @@ on 1 byte), but shoehorning those bytes into integers efficiently is messy.
 
 #define SELF_TEST 1
 
-#include "nhash.h"
-
 #include <stdio.h>      /* defines printf for tests */
 #include <time.h>       /* defines time_t for timings in the test */
+#include "nhash.h"
 //#include <sys/param.h>  /* attempt to define endianness */
 //#ifdef linux
 //# include <endian.h>    /* attempt to define endianness */
