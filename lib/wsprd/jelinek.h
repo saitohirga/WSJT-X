4f7eb390871bd3ed05c0ea6f55a81490182e85d3
Steven Franke <s.j.franke@icloud.com>
2015-07-26 14:09:25 +0000

    Decrease memory footprint of stack and improve stack decoder efficiency.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@5723 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- lib/wsprd/jelinek.h
+++ lib/wsprd/jelinek.h
@@ -3,15 +3,14 @@
 
 #include <stdint.h>
 
-struct node {
+struct snode {
     uint64_t encstate; // Encoder state
-    uint64_t encstate_highbits;     // least significant 50 bits are the data
     int gamma;		         // Cumulative metric to this node
     unsigned int depth;               // depth of this node
     unsigned int jpointer;
 };
 
-struct node *stack;
+struct snode *stack;
 
 int jelinek(unsigned int *metric,
             unsigned int *cycles,
@@ -19,7 +18,7 @@ int jelinek(unsigned int *metric,
             unsigned char *symbols,
             unsigned int nbits,
             unsigned int stacksize,
-            struct node *stack,
+            struct snode *stack,
             int mettab[2][256],
             unsigned int maxcycles);
 
