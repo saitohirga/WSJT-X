saitohirga/WSJT-X

commit 3f75b4144adcae33ae0a4a2fd8fe7bcc43edeaf6

Send status information to  UDP server
To  facilitate interaction  with other  applications WSJT-X  now sends
status  updates  to  a  predefined   UDP  server  or  multicast  group
address. The  status updates include the  information currently posted
to  the  decodes.txt and  wsjtx_status.txt  files.   An optional  back
communications  channel is  also implemented  allowing the  UDP server
application to control some basic actions in WSJT-X.

A reference implementaion of a typical UDP server written in C++ using
Qt is  provided to demonstrate  these facilities. This  application is
not intended  as a user  tool but  only as an  example of how  a third
party application may interact with WSJT-X.

The  UDP messages  Use QDataStream  based serialization.  Messages are
documented in  NetworkMessage.hpp along with some  helper classes that
simplify the building and decoding of messages.

Two  message  handling  classes   are  introduced,  MessageClient  and
MessageServer.  WSJT-X uses the MessageClient class to manage outgoing
and  incoming  UDP  messages   that  allow  communication  with  other
applications.   The MessageServer  class implements  the kind  of code
that a  potential cooperating  application might use.   Although these
classes  use  Qt serialization  facilities,  the  message formats  are
easily  read and  written  by  applications that  do  not  use the  Qt
framework.

MessageAggregator   is   a   demonstration   application   that   uses
MessageServer and  presents a GUI  that displays messages from  one or
more  WSJT-X instances  and  allows sending  back a  CQ  or QRZ  reply
invocation  by double  clicking  a decode.   This  application is  not
intended as  a user facing tool  but rather as a  demonstration of the
WSJT-X UDP messaging facility. It  also demonstrates being a multicast
UDP server by allowing multiple instances to run concurrently. This is
enabled by using an appropriate  multicast group address as the server

logqso.cpp
@@ -50,7 +50,7 @@ void LogQSO::storeSettings () const
 
 void LogQSO::initLogQSO(QString hisCall, QString hisGrid, QString mode,
                         QString rptSent, QString rptRcvd, QDateTime dateTime,
-                        double dialFreq, QString myCall, QString myGrid,
+                        Radio::Frequency dialFreq, QString myCall, QString myGrid,
                         bool noSuffix, bool toRTTY, bool dBtoComments)
 {
   ui->call->setText(hisCall);
@@ -79,7 +79,7 @@ void LogQSO::initLogQSO(QString hisCall, QString hisGrid, QString mode,
   m_dialFreq=dialFreq;
   m_myCall=myCall;
   m_myGrid=myGrid;
-  QString band= ADIF::bandFromFrequency(dialFreq);
+  QString band= ADIF::bandFromFrequency(dialFreq / 1.e6);
   ui->band->setText(band);
 
   show ();
@@ -103,7 +103,7 @@ void LogQSO::accept()
   m_txPower=ui->txPower->text();
   comments=ui->comments->text();
   m_comments=comments;
-  QString strDialFreq(QString::number(m_dialFreq,'f',6));
+  QString strDialFreq(QString::number(m_dialFreq / 1.e6,'f',6));
 
   //Log this QSO to ADIF file "wsjtx_log.adi"
   QString filename = "wsjtx_log.adi";  // TODO allow user to set
@@ -125,26 +125,18 @@ void LogQSO::accept()
     m.exec();
   } else {
     QString logEntry=m_dateTime.date().toString("yyyy-MMM-dd,") +
-           m_dateTime.time().toString("hh:mm,") + hisCall + "," +
-           hisGrid + "," + strDialFreq + "," + mode +
-               "," + rptSent + "," + rptRcvd;
-       if(m_txPower!="") logEntry += "," + m_txPower;
-       if(comments!="") logEntry += "," + comments;
-       if(name!="") logEntry += "," + name;
+      m_dateTime.time().toString("hh:mm,") + hisCall + "," +
+      hisGrid + "," + strDialFreq + "," + mode +
+      "," + rptSent + "," + rptRcvd + "," + m_txPower +
+      "," + comments; + "," + name;
     QTextStream out(&f);
     out << logEntry << endl;
     f.close();
   }
 
 //Clean up and finish logging
-  Q_EMIT acceptQSO(true);
+  Q_EMIT acceptQSO (m_dateTime, hisCall, hisGrid, m_dialFreq, mode, rptSent, rptRcvd, m_txPower, comments, name);
   QDialog::accept();
-}
-
-void LogQSO::reject()
-{
-  Q_EMIT acceptQSO(false);
-  QDialog::reject();
 }
 
 // closeEvent is only called from the system menu close widget for a

CMakeLists.txt
@@ -198,6 +198,8 @@ set (wsjt_qt_CXXSRCS
   HamlibTransceiver.cpp
   HRDTransceiver.cpp
   DXLabSuiteCommanderTransceiver.cpp
+  NetworkMessage.cpp
+  MessageClient.cpp
   )
 
 set (jt9_CXXSRCS
@@ -372,11 +374,17 @@ set (wsjtx_UISRCS
   Configuration.ui
   )
 
+set (message_aggregator_CXXSRCS
+  MessageServer.cpp
+  MessageAggregator.cpp
+  )
+
 set (all_CXXSRCS
   ${wsjt_CXXSRCS}
   ${wsjt_qt_CXXSRCS}
   ${jt9_CXXSRCS}
   ${wsjtx_CXXSRCS}
+  ${message_aggregator_CXXSRCS}
   )
 
 set (all_C_and_CXXSRCS
@@ -850,6 +858,17 @@ set_target_properties (wsjtx PROPERTIES
 target_link_libraries (wsjtx wsjt_fort wsjt_cxx wsjt_qt ${hamlib_LIBRARIES} ${FFTW3_LIBRARIES})
 qt5_use_modules (wsjtx Widgets OpenGL Network Multimedia SerialPort)
 
+add_executable (message_aggregator
+  ${message_aggregator_CXXSRCS}
+  wsjtx.rc
+  )
+target_link_libraries (message_aggregator wsjt_qt)
+qt5_use_modules (message_aggregator Widgets OpenGL Network)
+
+if (WSJT_CREATE_WINMAIN)
+  set_target_properties (message_aggregator PROPERTIES WIN32_EXECUTABLE ON)
+endif (WSJT_CREATE_WINMAIN)
+
 set (SYSTEM_NAME ${CMAKE_SYSTEM_NAME}-${CMAKE_SYSTEM_PROCESSOR})
 if (WIN32)
   set (SYSTEM_NAME "${SYSTEM_NAME}i386")
@@ -887,7 +906,7 @@ install (TARGETS wsjtx
   BUNDLE DESTINATION . COMPONENT runtime
   )
 
-install (TARGETS jt9 jt65code jt9code
+install (TARGETS jt9 jt65code jt9code message_aggregator
   RUNTIME DESTINATION ${WSJT_BIN_DESTINATION} COMPONENT runtime
   BUNDLE DESTINATION ${WSJT_BIN_DESTINATION} COMPONENT runtime
   )

Configuration.cpp
@@ -131,6 +131,7 @@
 #include <iterator>
 #include <algorithm>
 #include <functional>
+#include <limits>
 
 #include <QApplication>
 #include <QMetaType>
@@ -148,6 +149,7 @@
 #include <QStringListModel>
 #include <QLineEdit>
 #include <QRegExpValidator>
+#include <QIntValidator>
 #include <QThread>
 #include <QTimer>
 #include <QStandardPaths>
@@ -169,6 +171,7 @@
 #include "Bands.hpp"
 #include "FrequencyList.hpp"
 #include "StationList.hpp"
+#include "NetworkServerLookup.hpp"
 
 #include "pimpl_impl.hpp"
 
@@ -374,6 +377,7 @@ class Configuration::impl final
 
 public:
   using FrequencyDelta = Radio::FrequencyDelta;
+  using port_type = Configuration::port_type;
 
   explicit impl (Configuration * self, QSettings * settings, QWidget * parent);
   ~impl ();
@@ -562,6 +566,11 @@ private:
   bool disable_TX_on_73_;
   bool watchdog_;
   bool TX_messages_;
+  QString udp_server_name_;
+  port_type udp_server_port_;
+  bool accept_udp_requests_;
+  bool udpWindowToFront_;
+  bool udpWindowRestore_;
   DataMode data_mode_;
 
   QAudioDeviceInfo audio_input_device_;
@@ -631,6 +640,11 @@ bool Configuration::split_mode () const
 {
   return !m_->rig_is_dummy_ && m_->rig_params_.split_mode_ != TransceiverFactory::split_mode_none;
 }
+QString Configuration::udp_server_name () const {return m_->udp_server_name_;}
+auto Configuration::udp_server_port () const -> port_type {return m_->udp_server_port_;}
+bool Configuration::accept_udp_requests () const {return m_->accept_udp_requests_;}
+bool Configuration::udpWindowToFront () const {return m_->udpWindowToFront_;}
+bool Configuration::udpWindowRestore () const {return m_->udpWindowRestore_;}
 Bands * Configuration::bands () {return &m_->bands_;}
 StationList * Configuration::stations () {return &m_->stations_;}
 FrequencyList * Configuration::frequencies () {return &m_->frequencies_;}
@@ -846,6 +860,9 @@ Configuration::impl::impl (Configuration * self, QSettings * settings, QWidget *
   ui_->grid_line_edit->setValidator (new QRegExpValidator {QRegExp {"[A-Ra-r]{2,2}[0-9]{2,2}[A-Xa-x]{0,2}"}, this});
   ui_->add_macro_line_edit->setValidator (new QRegExpValidator {message_alphabet, this});
 
+  ui_->udp_server_port_spin_box->setMinimum (1);
+  ui_->udp_server_port_spin_box->setMaximum (std::numeric_limits<port_type>::max ());
+
   //
   // assign ids to radio buttons
   //
@@ -1035,6 +1052,11 @@ void Configuration::impl::initialise_models ()
   ui_->CAT_RTS_check_box->setChecked (rig_params_.CAT_RTS_high_);
   ui_->TX_audio_source_button_group->button (rig_params_.TX_audio_source_)->setChecked (true);
   ui_->CAT_poll_interval_spin_box->setValue (rig_params_.CAT_poll_interval_);
+  ui_->udp_server_line_edit->setText (udp_server_name_);
+  ui_->udp_server_port_spin_box->setValue (udp_server_port_);
+  ui_->accept_udp_requests_check_box->setChecked (accept_udp_requests_);
+  ui_->udpWindowToFront->setChecked(udpWindowToFront_);
+  ui_->udpWindowRestore->setChecked(udpWindowRestore_);
 
   if (rig_params_.PTT_port_.isEmpty ())
     {
@@ -1211,6 +1233,11 @@ void Configuration::impl::read_settings ()
   TX_messages_ = settings_->value ("Tx2QSO", false).toBool ();
   rig_params_.CAT_poll_interval_ = settings_->value ("Polling", 0).toInt ();
   rig_params_.split_mode_ = settings_->value ("SplitMode", QVariant::fromValue (TransceiverFactory::split_mode_none)).value<TransceiverFactory::SplitMode> ();
+  udp_server_name_ = settings_->value ("UDPServer", "localhost").toString ();
+  udp_server_port_ = settings_->value ("UDPServerPort", 2237).toUInt ();
+  accept_udp_requests_ = settings_->value ("AcceptUDPRequests", false).toBool ();
+  udpWindowToFront_ = settings_->value ("udpWindowToFront",false).toBool ();
+  udpWindowRestore_ = settings_->value ("udpWindowRestore",false).toBool ();
 }
 
 void Configuration::impl::write_settings ()
@@ -1288,6 +1315,11 @@ void Configuration::impl::write_settings ()
   settings_->setValue ("TXAudioSource", QVariant::fromValue (rig_params_.TX_audio_source_));
   settings_->setValue ("Polling", rig_params_.CAT_poll_interval_);
   settings_->setValue ("SplitMode", QVariant::fromValue (rig_params_.split_mode_));
+  settings_->setValue ("UDPServer", udp_server_name_);
+  settings_->setValue ("UDPServerPort", udp_server_port_);
+  settings_->setValue ("AcceptUDPRequests", accept_udp_requests_);
+  settings_->setValue ("udpWindowToFront", udpWindowToFront_);
+  settings_->setValue ("udpWindowRestore", udpWindowRestore_);
 }
 
 void Configuration::impl::set_rig_invariants ()
@@ -1638,6 +1670,24 @@ void Configuration::impl::accept ()
   TX_messages_ = ui_->TX_messages_check_box->isChecked ();
   data_mode_ = static_cast<DataMode> (ui_->TX_mode_button_group->checkedId ());
   save_directory_ = ui_->save_path_display_label->text ();
+
+  auto new_server = ui_->udp_server_line_edit->text ();
+  if (new_server != udp_server_name_)
+    {
+      udp_server_name_ = new_server;
+      Q_EMIT self_->udp_server_changed (new_server);
+    }
+
+  auto new_port = ui_->udp_server_port_spin_box->value ();
+  if (new_port != udp_server_port_)
+    {
+      udp_server_port_ = new_port;
+      Q_EMIT self_->udp_server_port_changed (new_port);
+    }
+  
+  accept_udp_requests_ = ui_->accept_udp_requests_check_box->isChecked ();
+  udpWindowToFront_ = ui_->udpWindowToFront->isChecked ();
+  udpWindowRestore_ = ui_->udpWindowRestore->isChecked ();
 
   if (macros_.stringList () != next_macros_.stringList ())
     {
@@ -2425,7 +2475,6 @@ void Configuration::impl::fill_port_combo_box (QComboBox * cb)
   cb->addItem("USB");
   cb->setEditText (current_text);
 }
-
 
 inline
 bool operator != (RigParams const& lhs, RigParams const& rhs)

Configuration.hpp
@@ -19,6 +19,7 @@ class Bands;
 class FrequencyList;
 class StationList;
 class QStringListModel;
+class QHostAddress;
 
 //
 // Class Configuration
@@ -60,6 +61,7 @@ public:
   using MODE = Transceiver::MODE;
   using TransceiverState = Transceiver::TransceiverState;
   using Frequency = Radio::Frequency;
+  using port_type = quint16;
 
   enum DataMode {data_mode_none, data_mode_USB, data_mode_data};
   enum Type2MsgGen {type_2_msg_1_full, type_2_msg_3_full, type_2_msg_5_only};
@@ -105,6 +107,12 @@ public:
   bool watchdog () const;
   bool TX_messages () const;
   bool split_mode () const;
+  bool post_decodes () const;
+  QString udp_server_name () const;
+  port_type udp_server_port () const;
+  bool accept_udp_requests () const;
+  bool udpWindowToFront () const;
+  bool udpWindowRestore () const;
   Bands * bands ();
   FrequencyList * frequencies ();
   StationList * stations ();
@@ -165,6 +173,12 @@ public:
   // for the decoded text.
   //
   Q_SIGNAL void decoded_text_font_changed (QFont);
+
+  //
+  // This signal is emitted when the UDP server changes
+  //
+  Q_SIGNAL void udp_server_changed (QString const& udp_server);
+  Q_SIGNAL void udp_server_port_changed (port_type server_port);
 
 
   //

Configuration.ui
@@ -267,16 +267,6 @@
           <string>Behavior</string>
          </property>
          <layout class="QGridLayout" name="gridLayout_8">
-          <item row="3" column="0">
-           <widget class="QCheckBox" name="watchdog_check_box">
-            <property name="toolTip">
-             <string>Stop transmitting automatically after five periods.</string>
-            </property>
-            <property name="text">
-             <string>Runaway Tx &amp;watchdog</string>
-            </property>
-           </widget>
-          </item>
           <item row="2" column="0">
            <widget class="QCheckBox" name="disable_TX_on_73_check_box">
             <property name="toolTip">
@@ -285,6 +275,16 @@ text message.</string>
             </property>
             <property name="text">
              <string>Di&amp;sable Tx after sending 73</string>
+            </property>
+           </widget>
+          </item>
+          <item row="3" column="0">
+           <widget class="QCheckBox" name="watchdog_check_box">
+            <property name="toolTip">
+             <string>Stop transmitting automatically after five periods.</string>
+            </property>
+            <property name="text">
+             <string>Runaway Tx &amp;watchdog</string>
             </property>
            </widget>
           </item>
@@ -311,7 +311,7 @@ text message.</string>
             </property>
            </widget>
           </item>
-          <item row="4" column="0" colspan="2">
+          <item row="5" column="0" colspan="2">
            <layout class="QHBoxLayout" name="horizontalLayout_9">
             <item>
              <widget class="QCheckBox" name="CW_id_after_73_check_box">
@@ -370,16 +370,6 @@ quiet period when decoding is done.</string>
             </item>
            </layout>
           </item>
-          <item row="1" column="1">
-           <widget class="QCheckBox" name="tx_QSY_check_box">
-            <property name="toolTip">
-             <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Some rigs are not able to process CAT commands while transmitting. This means that if you are operating in split mode you may have to uncheck this option.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
-            </property>
-            <property name="text">
-             <string>Allow Tx frequency changes while transmitting</string>
-            </property>
-           </widget>
-          </item>
           <item row="0" column="1">
            <widget class="QCheckBox" name="monitor_last_used_check_box">
             <property name="toolTip">
@@ -387,6 +377,16 @@ quiet period when decoding is done.</string>
             </property>
             <property name="text">
              <string>Monitor returns to last used frequency</string>
+            </property>
+           </widget>
+          </item>
+          <item row="1" column="1">
+           <widget class="QCheckBox" name="tx_QSY_check_box">
+            <property name="toolTip">
+             <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Some rigs are not able to process CAT commands while transmitting. This means that if you are operating in split mode you may have to uncheck this option.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+            </property>
+            <property name="text">
+             <string>Allow Tx frequency changes while transmitting</string>
             </property>
            </widget>
           </item>
@@ -1627,10 +1627,10 @@ and DX Grid fields when a 73 or free text message is sent.</string>
        <item>
         <widget class="QGroupBox" name="network_group_box">
          <property name="title">
-          <string>Network</string>
+          <string>Network Services</string>
          </property>
-         <layout class="QVBoxLayout" name="verticalLayout_7">
-          <item>
+         <layout class="QGridLayout" name="gridLayout_17">
+          <item row="0" column="0">
            <widget class="QCheckBox" name="psk_reporter_check_box">
             <property name="toolTip">
              <string>The program can send your station details and all
@@ -1642,6 +1642,105 @@ for assessing propagation and system performance.</string>
              <string>Enable &amp;PSK Reporter Spotting</string>
             </property>
            </widget>
+          </item>
+         </layout>
+        </widget>
+       </item>
+       <item>
+        <widget class="QGroupBox" name="groupBox_4">
+         <property name="title">
+          <string>UDP Server</string>
+         </property>
+         <layout class="QGridLayout" name="gridLayout_16">
+          <item row="0" column="0">
+           <layout class="QFormLayout" name="formLayout_6">
+            <property name="fieldGrowthPolicy">
+             <enum>QFormLayout::AllNonFixedFieldsGrow</enum>
+            </property>
+            <item row="0" column="0">
+             <widget class="QLabel" name="udp_server_label">
+              <property name="text">
+               <string>UDP Server:</string>
+              </property>
+              <property name="buddy">
+               <cstring>udp_server_line_edit</cstring>
+              </property>
+             </widget>
+            </item>
+            <item row="0" column="1">
+             <widget class="QLineEdit" name="udp_server_line_edit">
+              <property name="sizePolicy">
+               <sizepolicy hsizetype="Preferred" vsizetype="Fixed">
+                <horstretch>0</horstretch>
+                <verstretch>0</verstretch>
+               </sizepolicy>
+              </property>
+              <property name="toolTip">
+               <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Optional hostname of network service to receive decodes.&lt;/p&gt;&lt;p&gt;Formats:&lt;/p&gt;&lt;ul style=&quot;margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px; -qt-list-indent: 1;&quot;&gt;&lt;li style=&quot; margin-top:12px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;hostname&lt;/li&gt;&lt;li style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;IPv4 address&lt;/li&gt;&lt;li style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;IPv6 address&lt;/li&gt;&lt;li style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;IPv4 multicast group address&lt;/li&gt;&lt;li style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;IPv6 multicast group address&lt;/li&gt;&lt;/ul&gt;&lt;p&gt;Clearing this field will disable the broadcasting of UDP status updates.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+              </property>
+              <property name="inputMethodHints">
+               <set>Qt::ImhDigitsOnly</set>
+              </property>
+             </widget>
+            </item>
+            <item row="1" column="0">
+             <widget class="QLabel" name="label_6">
+              <property name="text">
+               <string>UDP Server port number:</string>
+              </property>
+              <property name="buddy">
+               <cstring>udp_server_port_spin_box</cstring>
+              </property>
+             </widget>
+            </item>
+            <item row="1" column="1">
+             <widget class="QSpinBox" name="udp_server_port_spin_box">
+              <property name="toolTip">
+               <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Enter the service port number of the UDP server that WSJT-X should send updates to. If this is zero no updates will be broadcast.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+              </property>
+              <property name="minimum">
+               <number>0</number>
+              </property>
+              <property name="maximum">
+               <number>65534</number>
+              </property>
+             </widget>
+            </item>
+           </layout>
+          </item>
+          <item row="0" column="1">
+           <layout class="QVBoxLayout" name="verticalLayout_11">
+            <item>
+             <widget class="QCheckBox" name="accept_udp_requests_check_box">
+              <property name="toolTip">
+               <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;With this enabled WSJT-X will accept certain requests back from a UDP server that receives decode messages.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+              </property>
+              <property name="text">
+               <string>Accept UDP requests</string>
+              </property>
+             </widget>
+            </item>
+            <item>
+             <widget class="QCheckBox" name="udpWindowToFront">
+              <property name="toolTip">
+               <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Indicate acceptance of an incoming UDP request. The effect of this option varies depending on the operating system and window manager, its intent is to notify the acceptance of an incoming UDP request even if this application is minimized or hidden.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+              </property>
+              <property name="text">
+               <string>Notify on accepted UDP request</string>
+              </property>
+             </widget>
+            </item>
+            <item>
+             <widget class="QCheckBox" name="udpWindowRestore">
+              <property name="toolTip">
+               <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Restore the window from minimized if an UDP request is accepted.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+              </property>
+              <property name="text">
+               <string>Accepted UDP request restores window</string>
+              </property>
+             </widget>
+            </item>
+           </layout>
           </item>
          </layout>
         </widget>
@@ -1986,7 +2085,6 @@ soundcard changes</string>
   <tabstop>decoded_text_font_push_button</tabstop>
   <tabstop>monitor_off_check_box</tabstop>
   <tabstop>monitor_last_used_check_box</tabstop>
-  <tabstop>tx_QSY_check_box</tabstop>
   <tabstop>quick_call_check_box</tabstop>
   <tabstop>tx_QSY_check_box</tabstop>
   <tabstop>disable_TX_on_73_check_box</tabstop>
@@ -2038,6 +2136,11 @@ soundcard changes</string>
   <tabstop>report_in_comments_check_box</tabstop>
   <tabstop>clear_DX_check_box</tabstop>
   <tabstop>psk_reporter_check_box</tabstop>
+  <tabstop>udp_server_line_edit</tabstop>
+  <tabstop>udp_server_port_spin_box</tabstop>
+  <tabstop>accept_udp_requests_check_box</tabstop>
+  <tabstop>udpWindowToFront</tabstop>
+  <tabstop>udpWindowRestore</tabstop>
   <tabstop>frequencies_table_view</tabstop>
   <tabstop>stations_table_view</tabstop>
   <tabstop>pbCQmsg</tabstop>
@@ -2055,8 +2158,8 @@ soundcard changes</string>
    <slot>accept()</slot>
    <hints>
     <hint type="sourcelabel">
-     <x>236</x>
-     <y>540</y>
+     <x>245</x>
+     <y>605</y>
     </hint>
     <hint type="destinationlabel">
      <x>157</x>
@@ -2071,28 +2174,12 @@ soundcard changes</string>
    <slot>reject()</slot>
    <hints>
     <hint type="sourcelabel">
-     <x>304</x>
-     <y>540</y>
+     <x>313</x>
+     <y>605</y>
     </hint>
     <hint type="destinationlabel">
      <x>286</x>
      <y>274</y>
-    </hint>
-   </hints>
-  </connection>
-  <connection>
-   <sender>add_macro_line_edit</sender>
-   <signal>returnPressed()</signal>
-   <receiver>add_macro_push_button</receiver>
-   <slot>setFocus()</slot>
-   <hints>
-    <hint type="sourcelabel">
-     <x>188</x>
-     <y>62</y>
-    </hint>
-    <hint type="destinationlabel">
-     <x>406</x>
-     <y>62</y>
     </hint>
    </hints>
   </connection>
@@ -2103,11 +2190,27 @@ soundcard changes</string>
    <slot>setFocus()</slot>
    <hints>
     <hint type="sourcelabel">
-     <x>406</x>
+     <x>576</x>
      <y>62</y>
     </hint>
     <hint type="destinationlabel">
-     <x>188</x>
+     <x>199</x>
+     <y>60</y>
+    </hint>
+   </hints>
+  </connection>
+  <connection>
+   <sender>add_macro_line_edit</sender>
+   <signal>returnPressed()</signal>
+   <receiver>add_macro_push_button</receiver>
+   <slot>setFocus()</slot>
+   <hints>
+    <hint type="sourcelabel">
+     <x>199</x>
+     <y>60</y>
+    </hint>
+    <hint type="destinationlabel">
+     <x>576</x>
      <y>62</y>
     </hint>
    </hints>

commit 234b3b9824159dc058c4d3d429c06f4e619193aa

Fix defect in QSO log where name is not logged
This was  a regression introduced in  r5225, thanks to John  G4KLA for
spotting it.

git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@6029 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

logqso.cpp
@@ -128,7 +128,7 @@ void LogQSO::accept()
       m_dateTime.time().toString("hh:mm,") + hisCall + "," +
       hisGrid + "," + strDialFreq + "," + mode +
       "," + rptSent + "," + rptRcvd + "," + m_txPower +
-      "," + comments; + "," + name;
+      "," + comments + "," + name;
     QTextStream out(&f);
     out << logEntry << endl;
     f.close();