4bcc4f35a1e4d79076988265124f13585cd90230
Joe Taylor <k1jt@arrl.org>
2016-05-19 19:19:47 +0000

    Much improved detection of sync in JT4 decoder.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@6686 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- lib/xcor4.f90
+++ lib/xcor4.f90
@@ -1,18 +1,44 @@
-subroutine xcor4(s2,ipk,nsteps,nsym,ich,mode4)
+subroutine xcor4(s2,ipk,nsteps,nsym,lag1,lag2,ich,mode4,ccf,ccf0,   &
+     lagpk,flip)
 
-! Computes ccf of the 4-FSK spectral array s2 and the pseudo-random 
+! Computes ccf of the 4_FSK spectral array s2 and the pseudo-random 
 ! array pr2.  Returns peak of CCF and the lag at which peak occurs.  
 ! The CCF peak may be either positive or negative, with negative
-! implying a message with report.
+! implying the "OOO" message.
 
-  use jt4
   parameter (NHMAX=1260)           !Max length of power spectra
   parameter (NSMAX=525)            !Max number of half-symbol steps
   real s2(NHMAX,NSMAX)             !2d spectrum, stepped by half-symbols
   real a(NSMAX)
+  real ccf(-5:540)
+  integer nch(7)
+  integer npr2(207)
+  real pr2(207)
+  logical first
+  data lagmin/0/                    !Silence compiler warning
+  data first/.true./
+  data npr2/                                                        &
+       0,0,0,0,1,1,0,0,0,1,1,0,1,1,0,0,1,0,1,0,0,0,0,0,0,0,1,1,0,0, &
+       0,0,0,0,0,0,0,0,0,0,1,0,1,1,0,1,1,0,1,0,1,1,1,1,1,0,1,0,0,0, &
+       1,0,0,1,0,0,1,1,1,1,1,0,0,0,1,0,1,0,0,0,1,1,1,1,0,1,1,0,0,1, &
+       0,0,0,1,1,0,1,0,1,0,1,0,1,0,1,1,1,1,1,0,1,0,1,0,1,1,0,1,0,1, &
+       0,1,1,1,0,0,1,0,1,1,0,1,1,1,1,0,0,0,0,1,1,0,1,1,0,0,0,1,1,1, &
+       0,1,1,1,0,1,1,1,0,0,1,0,0,0,1,1,0,1,1,0,0,1,0,0,0,1,1,1,1,1, &
+       1,0,0,1,1,0,0,0,0,1,1,0,0,0,1,0,1,1,0,1,1,1,1,0,1,0,1/
+  data nch/1,2,4,9,18,36,72/
   save
 
+  if(first) then
+     do i=1,207
+        pr2(i)=2*npr2(i)-1
+     enddo
+     first=.false.
+  endif
+
+  ccfmax=0.
+  ccfmin=0.
   nw=nch(ich)
+
   do j=1,nsteps
      n=2*mode4
      if(mode4.eq.1) then
@@ -36,14 +62,34 @@ subroutine xcor4(s2,ipk,nsteps,nsym,ich,mode4)
      endif
   enddo
 
-  do lag=1,65
+  do lag=lag1,lag2
      x=0.
      do i=1,nsym
         j=2*i-1+lag
-        if(j.ge.1 .and. j.le.nsteps) x=x+a(j)*float(2*npr(i)-1)
+        if(j.ge.1 .and. j.le.nsteps) x=x+a(j)*pr2(i)
      enddo
-     zz(ipk,lag,ich)=x
+     ccf(lag)=2*x                        !The 2 is for plotting scale
+     if(ccf(lag).gt.ccfmax) then
+        ccfmax=ccf(lag)
+        lagpk=lag
+     endif
+
+     if(ccf(lag).lt.ccfmin) then
+        ccfmin=ccf(lag)
+        lagmin=lag
+     endif
   enddo
+
+  ccf0=ccfmax
+  flip=1.0
+  if(-ccfmin.gt.ccfmax) then
+     do lag=lag1,lag2
+        ccf(lag)=-ccf(lag)
+     enddo
+     lagpk=lagmin
+     ccf0=-ccfmin
+     flip=-1.0
+  endif
 
   return
 end subroutine xcor4
