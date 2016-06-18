0bc613e764ddbe968d7c7ac6c2ed01c58b0a2ba8
Joe Taylor <k1jt@arrl.org>
2016-06-18 19:37:47 +0000

    Update the "About" screen.  Add reference and link to GPL v3.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@6781 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- about.cpp
+++ about.cpp
@@ -19,13 +19,16 @@ CAboutDlg::CAboutDlg(QWidget *parent) :
                              + QCoreApplication::applicationVersion ()
                              + " " + revision ()}.simplified ()
                          + "</h2>\n\n"
-                         "WSJT-X implements digital modes JT9 and JT65 for <br>"
-                         "Amateur Radio communication.  <br><br>"
+                         "WSJT-X implements a number of digital modes designed for <br>"
+                         "weak-signal Amateur Radio communication.  <br><br>"
                          "&copy; 2001-2016 by Joe Taylor, K1JT, with grateful <br>"
                          "acknowledgment for contributions from AC6SL, AE4JY, <br>"
                          "DJ0OT, G4KLA, G4WJS, IW3RAB, K3WYC, K9AN, KA6MAL, <br>"
                          "KA9Q, KB1ZMX, KD6EKQ, KI7MT, KK1D, ND0B, PY2SDR, <br>"
-                         "VK3ACF, VK4BDJ, W4TI, W4TV, and W9MDB.<br>");
+                         "VK3ACF, VK4BDJ, W4TI, W4TV, and W9MDB.<br><br>"
+                         "WSJT-X is licensed under the terms of Version 3 <br>"
+                         "of the GNU General Public License (GPL) <br>"
+                         "https://www.gnu.org/licenses/gpl-3.0.txt");
 }
 
 CAboutDlg::~CAboutDlg()
