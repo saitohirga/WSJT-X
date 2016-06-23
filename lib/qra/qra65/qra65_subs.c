e2ea412e2456d31ea5ce400f829b336263975b41
Joe Taylor <k1jt@arrl.org>
2016-06-23 15:50:59 +0000

    Minor tweaks to qra65code to facilitate simple simulations.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@6803 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- lib/qra/qra65/qra65_subs.c
+++ lib/qra/qra65/qra65_subs.c
@@ -22,8 +22,8 @@ void qra65_dec_(float r[], int xdec[], int* rc)
 //   rc=4    [CALL ?     ] AP45
 //   rc=5    [CALL CALL ?] AP57
 
-  //  int ncall=0xf70c238;                          //K1ABC
-  int ncall=0x890c60c;                          //KA1ABC
+  int ncall=0xf70c238;                          //K1ABC
+  //  int ncall=0x890c60c;                          //KA1ABC
   int i;
   qra65codec *codec = qra65_init(1,ncall);	//codec for ncall
   *rc = qra65_decode(codec,xdec,r);
