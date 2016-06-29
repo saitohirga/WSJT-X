9a9e5c3d9074be74846a30b66b1bdc6a407464d7
Joe Taylor <k1jt@arrl.org>
2016-06-29 19:12:46 +0000

    More changes for QRA01 --> QRA02.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@6825 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- mainwindow.h
+++ mainwindow.h
@@ -42,7 +42,7 @@
 #define NUM_ISCAT_SYMBOLS 1291             //30*11025/256
 #define NUM_JTMSK_SYMBOLS 234              //(72+15+12)*2 + 3*11 sync + 3 f0-parity
 #define NUM_MSK144_SYMBOLS 144             //s8 + d48 + s8 + d80
-#define NUM_QRA65_SYMBOLS 126                //63 data + 63 sync
+#define NUM_QRA65_SYMBOLS 84               //63 data + 21 sync
 
 #define NUM_CW_SYMBOLS 250
 #define TX_SAMPLE_RATE 48000
