62a668f6d62760aa50972466474b4a51c0ab33b9
Joe Taylor <k1jt@arrl.org>
2016-06-29 19:10:57 +0000

    Starting the transition from QRA01 to QRA02.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@6824 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- lib/genqra65.f90
+++ lib/genqra65.f90
@@ -1,23 +1,17 @@
 subroutine genqra65(msg0,ichk,msgsent,itone,itype)
 
-! Encodes a QRA65 message to yieild itone(1:126)
+! Encodes a QRA65 message to yield itone(1:84)
 
   use packjt
   character*22 msg0
   character*22 message          !Message to be generated
   character*22 msgsent          !Message as it will be received
-  integer itone(126)
+  integer itone(84)
   character*3 cok               !'   ' or 'OOO'
   integer dgen(13)
   integer sent(63)
-  integer nprc(126)
-  data nprc/1,0,0,1,1,0,0,0,1,1,1,1,1,1,0,1,0,1,0,0,  &
-            0,1,0,1,1,0,0,1,0,0,0,1,1,1,0,0,1,1,1,1,  &
-            0,1,1,0,1,1,1,1,0,0,0,1,1,0,1,0,1,0,1,1,  &
-            0,0,1,1,0,1,0,1,0,1,0,0,1,0,0,0,0,0,0,1,  &
-            1,0,0,0,0,0,0,0,1,1,0,1,0,0,1,0,1,1,0,1,  &
-            0,1,0,1,0,0,1,1,0,0,1,0,0,1,0,0,0,0,1,1,  &
-            1,1,1,1,1,1/
+  integer icos7(0:6)
+  data icos7/2,5,6,0,4,1,3/     !Defines a 7x7 Costas array
   save
 
   if(msg0(1:1).eq.'@') then
@@ -40,36 +34,16 @@ subroutine genqra65(msg0,ichk,msgsent,itone,itype)
      enddo
 
      call chkmsg(message,cok,nspecial,flip)
-     if(nspecial.eq.0) then
-        call packmsg(message,dgen,itype)    !Pack message into 72 bits
-        call unpackmsg(dgen,msgsent)        !Unpack to get message sent
-        if(ichk.ne.0) go to 999             !Return if checking only
+     call packmsg(message,dgen,itype)    !Pack message into 72 bits
+     call unpackmsg(dgen,msgsent)        !Unpack to get message sent
+     if(ichk.ne.0) go to 999             !Return if checking only
+     call qra65_enc(dgen,sent)           !Encode using QRA65
 
-        call qra65_enc(dgen,sent)           !Encode using QRA65
-        nsym=126                            !Symbols per transmission
-        k=0
-        do j=1,nsym
-           if(nprc(j).eq.0) then
-              k=k+1
-              itone(j)=sent(k)+2
-           else
-              itone(j)=0
-           endif
-        enddo
-     else
-        nsym=32
-        k=0
-        do j=1,nsym
-           do n=1,4
-              k=k+1
-              if(iand(j,1).eq.1) itone(k)=0
-              if(iand(j,1).eq.0) itone(k)=10*nspecial
-              if(k.eq.126) go to 10
-           enddo
-        enddo
-10      msgsent=message
-        itype=7
-     endif
+     itone(1:7)=icos7                    !Insert 7x7 Costas array in 3 places
+     itone(8:39)=sent(1:32)
+     itone(40:46)=icos7
+     itone(47:77)=sent(33:63)
+     itone(78:84)=icos7
   endif
 
 999 return
