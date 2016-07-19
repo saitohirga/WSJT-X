87ada9011a89229f94d6a96362f4a78fa06ad4ce
Joe Taylor <k1jt@arrl.org>
2016-07-19 15:24:15 +0000

    Try APtype 4, 3, 2, 1, 0.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@6929 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- lib/qra/qra64/qra64_subs.c
+++ lib/qra/qra64/qra64_subs.c
@@ -10,44 +10,48 @@ static qra64codec *pqra64codec = NULL;
 
 void qra64_enc_(int x[], int y[])
 {
-  if (pqra64codec==NULL) pqra64codec = qra64_init(QRA_AUTOAP);  
+  if (pqra64codec==NULL) pqra64codec = qra64_init(QRA_USERAP);  
   qra64_encode(pqra64codec, y, x);
 }
 
-void qra64_dec_(float r[], int* nmycall, int* nhiscall, int* nhisgrid, 
+void qra64_dec_(float r[], int* nc1, int* nc2, int* ng2, int* APtype, 
 		int xdec[], float* snr, int* rc)
 {
-// Return codes:
-//   rc=-16  failed sanity check
-//   rc=-2   decoded, but crc check failed
-//   rc=-1   no decode
-//   rc=0    [?    ?    ?] AP0	(decoding with no a-priori information)
-//   rc=1    [CQ   ?    ?] AP27
-//   rc=2    [CQ   ?     ] AP42
-//   rc=3    [CALL ?    ?] AP29
-//   rc=4    [CALL ?     ] AP44
-//   rc=5    [CALL CALL ?] AP57
-//   rc=6    [?    CALL ?] AP29
-//   rc=7    [?    CALL  ] AP44
-//   rc=8    [CALL CALL G] AP72
+/*
+  APtype:
+     -1                         (no AP information)
+      0    [CQ/QRZ  *     ?_]   (* means 26 or 28 bit info)
+      1    [call1   *     ?_]   (?_ means 16-bit info or "blank")
+      2    [*     call2   ?_]
+      3    [call1 call2   ?_]
+      4    [call1 call2 grid]
 
-  static ncall0=-1;
-  int ncall=*nmycall;
+  Return codes:
+    -16    Failed sanity check
+     -2    Decoded, but crc check failed
+     -1    No decode
+      0    [?    ?    ?] AP0	(decoding with no a-priori information)
+      1    [CQ   ?    ?] AP27
+      2    [CQ   ?     ] AP42
+      3    [CALL ?    ?] AP29
+      4    [CALL ?     ] AP44
+      5    [CALL CALL ?] AP57
+      6    [?    CALL ?] AP29
+      7    [?    CALL  ] AP44
+      8    [CALL CALL G] AP72
+*/
+  static int nc1z=-1;
   float EbNodBEstimated;
+  int err=0;
 
 #ifdef NICO_WANTS_SNR_DUMP  
   FILE *fout;
 #endif
-  
-  if(ncall!=ncall0) {
-    if (pqra64codec!=NULL) qra64_close(pqra64codec);
-    pqra64codec = qra64_init(QRA_AUTOAP);
-    // the following apset call is not strictly necessary
-    // It enables AP decoding of messages directed to our call
-    // also in the case we have never made a CQ
-    qra64_apset(pqra64codec,ncall,0,0,APTYPE_MYCALL);
-    ncall0=ncall;
-  }
+
+  if(pqra64codec==NULL) pqra64codec = qra64_init(QRA_USERAP);
+  err=qra64_apset(pqra64codec,*nc1,*nc2,*ng2,*APtype);
+  if(err<0) printf("ERROR: qra64_apset returned %d\n",err);
+
   *rc = qra64_decode(pqra64codec,&EbNodBEstimated,xdec,r);
   *snr = EbNodBEstimated - 31.0;
 
