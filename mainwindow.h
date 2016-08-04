9993760bacdc9d4fce7bd517e5913ef635953de9
Joe Taylor <k1jt@arrl.org>
2016-08-04 19:30:27 +0000

    Re-enable MSK144 click-to-decode, top panel only. Fix the improper display of "Fast" checkbox. Disable "CQ nnn" checkbox if MyCall is not K1JT or K9AN. Use AppDir as path to execute the LDPC encode program. Don't display submodes in mode label for JTMSK, MSK144. Make Rx Freq spinner invisible in MSK144 mode.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@7001 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- mainwindow.h
+++ mainwindow.h
@@ -427,6 +427,7 @@ private:
   char    m_msg[100][80];
   char    m_pchkFile[512];
   char    m_ldpcMsgFile[512];
+  char    m_encodeExeFile[512];
 
   // labels in status bar
   QLabel tx_status_label;
