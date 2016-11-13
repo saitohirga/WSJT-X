94ee6ea8a1cd9703e9654e809af8f48e424659e1
Joe Taylor <k1jt@arrl.org>
2016-11-13 20:36:27 +0000

    Sorry, I did not intend to commit changes to these routines.  Revert to 7305.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@7307 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- lib/sync64.f90
+++ lib/sync64.f90
@@ -1,8 +1,7 @@
-subroutine sync64(dd,npts,nf1,nf2,nfqso,ntol,mode64,maxf1,dtx,f0,jpk,kpk,   &
-     snrdb,c0)
+subroutine sync64(dd,nf1,nf2,nfqso,ntol,mode64,maxf1,dtx,f0,jpk,kpk,snrdb,c0)
 
   parameter (NMAX=60*12000)                  !Max size of raw data at 12000 Hz
-  parameter (NSPS=3456)                      !Samples per symbol at 6000 Hz
+  parameter (NSPS=2304)                      !Samples per symbol at 4000 Hz
   parameter (NSPC=7*NSPS)                    !Samples per Costas array
   real dd(NMAX)                              !Raw data
   real s1(0:NSPC-1)                          !Power spectrum of Costas 1
@@ -15,7 +14,7 @@ subroutine sync64(dd,npts,nf1,nf2,nfqso,ntol,mode64,maxf1,dtx,f0,jpk,kpk,   &
   integer icos7(0:6)                         !Costas 7x7 tones
   integer ipk0(1)
   complex cc(0:NSPC-1)                       !Costas waveform
-  complex c0(0:720000)                       !Complex spectrum of dd()
+  complex c0(0:360000)                       !Complex spectrum of dd()
   complex c1(0:NSPC-1)                       !Complex spectrum of Costas 1
   complex c2(0:NSPC-1)                       !Complex spectrum of Costas 2
   complex c3(0:NSPC-1)                       !Complex spectrum of Costas 3
@@ -32,8 +31,8 @@ subroutine sync64(dd,npts,nf1,nf2,nfqso,ntol,mode64,maxf1,dtx,f0,jpk,kpk,   &
      k=-1
      phi=0.
      do j=0,6                               !Compute complex Costas waveform
-        dphi=twopi*nsync*icos7(j)*dfgen/6000.0
-        do i=1,NSPS
+        dphi=twopi*nsync*icos7(j)*dfgen/4000.0
+        do i=1,2304
            phi=phi + dphi
            if(phi.gt.twopi) phi=phi-twopi
            k=k+1
@@ -43,20 +42,20 @@ subroutine sync64(dd,npts,nf1,nf2,nfqso,ntol,mode64,maxf1,dtx,f0,jpk,kpk,   &
      mode64z=mode64
   endif
 
+  npts0=54*12000
   nfft1=672000
-  nfft2=nfft1/2
+  nfft2=nfft1/3
   df1=12000.0/nfft1
   fac=2.0/nfft1
-  c0(0:npts-1)=fac*dd(1:npts)
-  c0(npts:nfft1)=0.
-  call four2a(c0,nfft1,1,-1,1)             !Forward c2c FFT
-  c0(nfft2/2+1:nfft2)=0.
-  c0(0)=0.5*c0(0)
+  do i=0,nfft1/2                           !Load real data into c0
+     c0(i)=fac*cmplx(dd(1+2*i),dd(2+2*i))
+  enddo
+  call four2a(c0,nfft1,1,-1,0)             !Forward r2c FFT
   call four2a(c0,nfft2,1,1,1)              !Inverse c2c FFT; c0 is analytic sig
-  npts2=npts/2                            !Downsampled complex data length
+  npts2=npts0/3                            !Downsampled complex data length
   nfft3=NSPC
   nh3=nfft3/2
-  df3=6000.0/nfft3
+  df3=4000.0/nfft3
   fa=max(nf1,nfqso-ntol)
   fb=min(nf2,nfqso+ntol)
   ia=max(maxf1,nint(fa/df3))
@@ -65,7 +64,7 @@ subroutine sync64(dd,npts,nf1,nf2,nfqso,ntol,mode64,maxf1,dtx,f0,jpk,kpk,   &
   snr=0.
   jpk=0
   ja=0
-  jb=6*6000
+  jb=6*4000
   jstep=200
   ka=-maxf1
   kb=maxf1
@@ -73,8 +72,8 @@ subroutine sync64(dd,npts,nf1,nf2,nfqso,ntol,mode64,maxf1,dtx,f0,jpk,kpk,   &
   kpk=0
   do iter=1,2
      do j1=ja,jb,jstep
-        j2=j1 + 39*NSPS
-        j3=j1 + 77*NSPS
+        j2=j1 + 39*2304
+        j3=j1 + 77*2304
         c1=1.e-4*c0(j1:j1+NSPC-1) * conjg(cc)
         call four2a(c1,nfft3,1,-1,1)
         c2=1.e-4*c0(j2:j2+NSPC-1) * conjg(cc)
@@ -96,7 +95,7 @@ subroutine sync64(dd,npts,nf1,nf2,nfqso,ntol,mode64,maxf1,dtx,f0,jpk,kpk,   &
            s0b(:ia-1)=0.
            s0b(ib+1:)=0.
            nadd=(7*mode64)/2
-           if(mod(nadd,2).eq.0) nadd=nadd+1       !Make nadd odd
+           if(mod(nadd,2).eq.1) nadd=nadd+1       !Make nadd odd
            if(nadd.ge.3) call smo(s0b(ia:ib),iz,s0(ia:ib),nadd)
            call smo121(s0(ia:ib),iz)
            nskip=max(14,2*mode64)
@@ -106,7 +105,7 @@ subroutine sync64(dd,npts,nf1,nf2,nfqso,ntol,mode64,maxf1,dtx,f0,jpk,kpk,   &
               jpk=j1
               s0a=s0/rms
               snr=s
-              dtx=jpk/6000.0 - 1.0
+              dtx=jpk/4000.0 - 1.0
               ipk0=maxloc(s0(ia:ib))
               ipk=ipk0(1)
               f0=(ipk+ia-1)*df3
