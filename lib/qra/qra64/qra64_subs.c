4838d9737069c9642a556d1e5fb6d2f6b8492331
Joe Taylor <k1jt@arrl.org>
2016-11-27 15:50:47 +0000

    New parameters for AP decoding in QRA64.  Do not use this revision on the air!
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@7340 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- lib/qra/qra64/qra64_subs.c
+++ lib/qra/qra64/qra64_subs.c
@@ -4,8 +4,6 @@
 #include "qra64.h"
 #include <stdio.h>
 
-//#define NICO_WANTS_SNR_DUMP
-
 static qra64codec *pqra64codec = NULL;
 
 void qra64_enc_(int x[], int y[])
@@ -19,27 +17,33 @@ void qra64_dec_(float r[], int* nc1, int* nc2, int* ng2, int* APtype,
 		int xdec[], float* snr, int* rc)
 {
 /*
-  APtype:
-     -1                         (no AP information)
-      0    [CQ/QRZ  *     ?_]   (* means 26 or 28 bit info)
-      1    [call1   *     ?_]   (?_ means 16-bit info or "blank")
-      2    [*     call2   ?_]
-      3    [call1 call2   ?_]
-      4    [call1 call2 grid]
+  APtype:                         AP
+-----------------------------------------------------------------------
+     -1                            0     (no AP information)
+      0    [CQ/QRZ    ?      ? ]  25/37
+      1    [MyCall    ?      ? ]  25/37
+      2    [  ?    HisCall   ? ]  25/37
+      3    [MyCall HisCall   ? ]  49/68
+      4    [MyCall HisCall grid]  68
+      5    [CQ/QRZ HisCall   ? ]  49/68
 
-  Return codes:
-    -16    Failed sanity check
-     -2    Decoded, but crc check failed
-     -1    No decode
-      0    [?    ?    ?] AP0	(decoding with no a-priori information)
-      1    [CQ   ?    ?] AP27
-      2    [CQ   ?     ] AP42
-      3    [CALL ?    ?] AP29
-      4    [CALL ?     ] AP44
-      5    [CALL CALL ?] AP57
-      6    [?    CALL ?] AP29
-      7    [?    CALL  ] AP44
-      8    [CALL CALL G] AP72
+     rc    Message format         AP APTYPE Comments
+------------------------------------------------------------------------
+    -16                                     Failed sanity check
+     -2                                     Decoded but CRC failed
+     -1                                     No decode
+      0    [   ?      ?      ? ]   0   -1   Decode with no AP info
+      1    [CQ/QRZ    ?      ? ]  25    0
+      2    [CQ/QRZ    ?      _ ]  37    0
+      3    [MyCall    ?      ? ]  25    1
+      4    [MyCall    ?      _ ]  37    1
+      5    [MyCall HisCall   ? ]  49    3
+      6    [   ?   HisCall   ? ]  25    2   Optional
+      7    [   ?   HisCall   _ ]  37    2   Optional
+      8    [MyCall HisCall Grid]  68    4
+      9    [CQ/QRZ HisCall   ? ]  49    5   Optional (not needed?)
+     10    [CQ/QRZ HisCall   _ ]  68    5   Optional
+     11    [CQ/QRZ HisCall Grid]  68    ?   Optional
 */
   static int nc1z=-1;
   float EbNodBEstimated;
@@ -48,25 +52,14 @@ void qra64_dec_(float r[], int* nc1, int* nc2, int* ng2, int* APtype,
   float b90=*b0;
   int nFadingModel=*nf0;
 
-#ifdef NICO_WANTS_SNR_DUMP  
-  FILE *fout;
-#endif
-
   if(pqra64codec==NULL) pqra64codec = qra64_init(QRA_USERAP);
   err=qra64_apset(pqra64codec,*nc1,*nc2,*ng2,*APtype);
   if(err<0) printf("ERROR: qra64_apset returned %d\n",err);
 
   if(*iset==0) {
-    //    *rc = qra64_decode(pqra64codec,&EbNodBEstimated,xdec,r);
     *rc = qra64_decode_fastfading(pqra64codec,&EbNodBEstimated,xdec,r,
     				  nSubmode,b90,nFadingModel);
     *snr = EbNodBEstimated - 31.0;
-
-#ifdef NICO_WANTS_SNR_DUMP  
-    fout = fopen("C:\\JTSDK\\snrdump.txt","a+");
-    if ((*rc)>=0) fprintf(fout,"rc=%d snr=%.2f dB\n",*rc,EbNodBEstimated-31.0f);
-    fclose(fout);
-#endif
   }
 }
 
