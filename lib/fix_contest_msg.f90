4cd038f34246ba8498901661b99c2ab546b4e6f0
Joe Taylor <k1jt@arrl.org>
2016-12-18 02:46:04 +0000

    Make Contest Mode messages properly readable by anyone in the sender's hemisphere.  Add Contest Mode info to User Guide.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@7395 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- lib/fix_contest_msg.f90
+++ lib/fix_contest_msg.f90
@@ -5,25 +5,21 @@ subroutine fix_contest_msg(mycall,mygrid,hiscall,msg)
 ! of grid1.
 
   character*6 mycall,mygrid,hiscall
-  character*22 msg,t
+  character*22 msg
   character*6 g1,g2
   logical isgrid
 
-  t=trim(mycall)//' '//trim(hiscall)
-  i0=index(msg,trim(t))
-  if(i0.eq.1) then
-     i1=len(trim(t))+2
-     g1=msg(i1:i1+3)
-     if(isgrid(g1)) then
-        call azdist(mygrid,g1,0.d0,nAz,nEl,nDmiles,nDkm,nHotAz,nHotABetter)
-        if(ndkm.gt.10000) then
-           call grid2deg(g1,dlong,dlat)
-           dlong=dlong+180.0
-           if(dlong.gt.180.0) dlong=dlong-360.0
-           dlat=-dlat
-           call deg2grid(dlong,dlat,g2)
-           msg=msg(1:i1-1)//'R '//g2(1:4)
-        endif
+  n=len(trim(msg))
+  g1=msg(n-3:n)//'  '
+  if(isgrid(g1)) then
+     call azdist(mygrid,g1,0.d0,nAz,nEl,nDmiles,nDkm,nHotAz,nHotABetter)
+     if(ndkm.gt.10000) then
+        call grid2deg(g1,dlong,dlat)
+        dlong=dlong+180.0
+        if(dlong.gt.180.0) dlong=dlong-360.0
+        dlat=-dlat
+        call deg2grid(dlong,dlat,g2)
+        msg=msg(1:n-4)//'R '//g2(1:4)
      endif
   endif
 
