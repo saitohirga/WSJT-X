8189c437a0468d8f484c80d0595d7336deb29c7c
Bill Somerville <g4wjs@classdesign.com>
2016-12-19 03:18:38 +0000

    Adjust splash screen for GA release
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@7403 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- SplashScreen.cpp
+++ SplashScreen.cpp
@@ -26,17 +26,12 @@ SplashScreen::SplashScreen ()
   : QSplashScreen {QPixmap {":/splash.png"}, Qt::WindowStaysOnTopHint}
 {
   setLayout (&m_->main_layout_);
-  showMessage ("<h2>" + QString {"Alpha Release: WSJT-X v" +
+  showMessage ("<h2>" + QString {"WSJT-X v" +
         QCoreApplication::applicationVersion() + " " +
         revision ()}.simplified () + "</h2>"
-    "V1.7 has many new features, most aimed at VHF/UHF/Microwave users.<br />"
-    "Some are not yet described in the User Guide and may not be thoroughly<br />"
-    "tested. The release notes have more details.<br /><br />"
-    "As a test user you have an obligation to report anomalous results<br />"
-    "to the development team.  We are particularly interested in tests<br />"
-    "of experimental modes QRA64 (intended for EME) and MSK144<br />"
-    "(intended for meteor scatter).<br /><br />"
-    "Send reports to wsjtgroup@yahoogroups.com, and be sure to save .wav<br />"
+    "V1.7 has many new features, most aimed at VHF/UHF/Microwave users.<br /><br />"
+    "The release notes have more details.<br /><br />"
+    "Send issue reports to wsjtgroup@yahoogroups.com, and be sure to save .wav<br />"
     "files where appropriate.<br /><br />"
     "<b>Open the Help menu and select Release Notes for more details.</b><br />"
     "<img src=\":/icon_128x128.png\" />"
