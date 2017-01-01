b166ead8582704866245837819d0e2b1c6bc2745
Steven Franke <s.j.franke@icloud.com>
2017-01-01 16:45:47 +0000

    Tweak.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@7438 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- lib/update_hasharray.f90
+++ lib/update_hasharray.f90
@@ -7,7 +7,7 @@ subroutine update_hasharray(recent_calls,nrecent,nhasharray)
   nhasharray=-1
   do i=1,nrecent
     do j=i+1,nrecent
-      if( recent_calls(i) .ne. '  ' .and. recent_calls(j) .ne. '  ' ) then
+      if( recent_calls(i)(1:2) .ne. '  ' .and. recent_calls(j)(1:2) .ne. '  ' ) then
         hashmsg=trim(recent_calls(i))//' '//trim(recent_calls(j))
         call fmtmsg(hashmsg,iz)
         call hash(hashmsg,22,ihash)
