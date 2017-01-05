7e81042ab593cd2f658386627e0339fea32b0a3a
Steven Franke <s.j.franke@icloud.com>
2017-01-05 02:26:55 +0000

    More progress on SWL mode.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@7449 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- lib/msk40decodeframe.f90
+++ lib/msk40decodeframe.f90
@@ -135,30 +135,32 @@ subroutine msk40decodeframe(c,mycall,hiscall,xsnr,bswl,nhasharray,             &
     nrxhash=(imsg-nrxrpt)/16
 
     if(nhammd.le.4 .and. cord .lt. 0.65 .and. nrxhash.eq.ihash) then
-!write(*,*) 'decodeframe 1',nhammd,cord,nrxhash,nrxrpt,ihash,xsnr,sigma
+!write(*,*) 'decodeframe 1',nbadsync,nhammd,cord,nrxhash,nrxrpt,ihash,xsnr,sigma
       nsuccess=1    
       write(msgreceived,'(a1,a,1x,a,a1,1x,a4)') "<",trim(mycall),   &
                                     trim(hiscall),">",rpt(nrxrpt)
       return
-    elseif(bswl .and. nhammd.le.2 .and. cord.lt.0.40 .and. xsnr .gt. -3.0) then
+!    elseif(bswl .and. nhammd.le.2 .and. cord.lt.0.40 .and. xsnr .gt. -3.0) then
+    elseif(bswl .and. nhammd.le.4 .and. cord.lt.0.65 ) then
       do i=1,nrecent
         do j=i+1,nrecent
           if( nrxhash .eq. nhasharray(i,j) ) then
             nsuccess=2
             write(msgreceived,'(a1,a,1x,a,a1,1x,a4)') "<",trim(recent_calls(i)),   &
                                   trim(recent_calls(j)),">",rpt(nrxrpt)
-!write(*,*) 'decodeframe 2',nhammd,cord,nrxhash,nrxrpt,ihash,xsnr,sigma
+!write(*,*) 'decodeframe 2',nbadsync,nhammd,cord,nrxhash,nrxrpt,ihash,xsnr,sigma
           elseif( nrxhash .eq. nhasharray(j,i) ) then
             nsuccess=2
             write(msgreceived,'(a1,a,1x,a,a1,1x,a4)') "<",trim(recent_calls(j)),   &
                                   trim(recent_calls(i)),">",rpt(nrxrpt)
-!write(*,*) 'decodeframe 3',nhammd,cord,nrxhash,nrxrpt,ihash,xsnr,sigma
+!write(*,*) 'decodeframe 3',nbadsync,nhammd,cord,nrxhash,nrxrpt,ihash,xsnr,sigma
           endif
         enddo
       enddo
-      if(nsuccess.eq.0 .and. nhammd.le.2 .and. cord.lt.0.40 .and. xsnr.gt.0.0 ) then
-!write(*,*) 'decodeframe 4',bswl,nhammd,cord,nrxhash,nrxrpt,ihash,xsnr,sigma
+!      if(nsuccess.eq.0 .and. nhammd.le.2 .and. cord.lt.0.40 .and. xsnr.gt. -3.0 ) then
+      if(nsuccess.eq.0) then
         nsuccess=3
+!write(*,*) 'decodeframe 4',bswl,nbadsync,nhammd,cord,nrxhash,nrxrpt,ihash,xsnr,sigma,nsuccess
         write(msgreceived,'(a1,i4.4,a1,1x,a4)') "<",nrxhash,">",rpt(nrxrpt)
       endif
     endif 
