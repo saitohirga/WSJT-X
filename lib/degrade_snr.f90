99b1fb7b1b55218027d6b8aafc99567eee580124
Joe Taylor <k1jt@arrl.org>
2017-05-18 15:59:30 +0000

    Limit WSPR-LF frequency search range to 1600-1650 Hz.  Enable S/N degradation.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@7689 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- lib/degrade_snr.f90
+++ lib/degrade_snr.f90
@@ -1,15 +1,20 @@
 subroutine degrade_snr(d2,npts,db,bw)
 
-  integer*2 d2(npts)
-  real dat(60*12000)
+! Degrade S/N by specified number of dB.
 
-  dat(1:npts)=d2
-  p0=dot_product(dat(1:npts),dat(1:npts))/npts
+  integer*2 d2(npts)
+  
+  p0=0.
+  do i=1,npts
+     x=d2(i)
+     p0=p0 + x*x
+  enddo
+  p0=p0/npts
   if(bw.gt.0.0) p0=p0*6000.0/bw
   s=sqrt(p0*(10.0**(0.1*db) - 1.0))
   fac=sqrt(p0/(p0+s*s))
   do i=1,npts
-     d2(i)=nint(fac*(dat(i) + s*gran()))
+     d2(i)=nint(fac*(d2(i) + s*gran()))
   enddo
 
   return
