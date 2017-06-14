44b4c180f65d2a200ae3fce58bc766dbaaa079f4
Joe Taylor <k1jt@arrl.org>
2017-06-14 20:21:01 +0000

    First bare-bones decoder for FT8.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@7716 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- lib/fsk4hf/genft8.f90
+++ lib/fsk4hf/genft8.f90
@@ -8,8 +8,8 @@ subroutine genft8(msg,msgsent,itone)
   include 'ft8_params.f90'
 
   character*22 msg,msgsent
-  character*84 cbits
-  integer*4 i4Msg6BitWords(12)            !72-bit message as 6-bit words
+  character*87 cbits
+  integer*4 i4Msg6BitWords(12)                !72-bit message as 6-bit words
   integer*1 msgbits(KK),codeword(3*ND)
   integer itone(NN)
   integer icos7(0:6)
@@ -17,24 +17,26 @@ subroutine genft8(msg,msgsent,itone)
 
   call packmsg(msg,i4Msg6BitWords,itype)      !Pack into 12 6-bit bytes
   call unpackmsg(i4Msg6BitWords,msgsent)      !Unpack to get msgsent
-  icrc=0        !### temporary ###
+  i3bit=0         !### temporary ###
+  icrc12=0        !### temporary ###
 
-  write(cbits,1000) i4Msg6BitWords,icrc
-1000 format(12b6.6,b12.12)
+  write(cbits,1000) i4Msg6BitWords,i3bit,icrc12
+1000 format(12b6.6,b3.3,b12.12)
   read(cbits,1002) msgbits
-1002 format(84i1)
+1002 format(87i1)
+  print*,cbits
 
-  call encode168(msgbits,codeword)      !Encode the test message
+  call encode174(msgbits,codeword)      !Encode the test message
 
-! Message structure: S7 D28 S7 D28 S7
+! Message structure: S7 D29 S7 D29 S7
   itone(1:7)=icos7
-  itone(35+1:35+7)=icos7
+  itone(36+1:36+7)=icos7
   itone(NN-6:NN)=icos7
   k=7
   do j=1,ND
      i=3*j -2
      k=k+1
-     if(j.eq.29) k=k+7
+     if(j.eq.30) k=k+7
      itone(k)=codeword(i)*4 + codeword(i+1)*2 + codeword(i+2)
   enddo
 
