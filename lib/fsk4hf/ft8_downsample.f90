52d8324382e37a1c1eb7c4257287dfd7fef15aff
Joe Taylor <k1jt@arrl.org>
2017-07-06 12:07:17 +0000

    Baseline code with nsps=2048 in FT8.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@7783 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

---
+++ lib/fsk4hf/ft8_downsample.f90
@@ -0,0 +1,41 @@
+subroutine ft8_downsample(dd,newdat,f0,c1)
+
+! Downconvert to complex data sampled at 187.5 Hz, 32 samples/symbol
+
+  parameter (NMAX=15*12000)
+  parameter (NFFT1=200000,NFFT2=3125)      !200000/64 = 3125
+  logical newdat
+  complex c1(0:NFFT2-1)
+  complex cx(0:NFFT1/2)
+  real dd(NMAX),x(NFFT1)
+  equivalence (x,cx)
+  save cx
+
+  if(newdat) then
+! Data in dd have changed, recompute the long FFT     
+     x(1:NMAX)=dd
+     x(NMAX+1:NFFT1)=0.                       !Zero-pad the x array
+     call four2a(cx,NFFT1,1,-1,0)             !r2c FFT to freq domain
+     newdat=.false.
+  endif
+
+  df=12000.0/NFFT1
+  baud=12000.0/2048.0
+  i0=nint(f0/df)
+  ft=f0+8.0*baud
+  it=min(nint(ft/df),NFFT1/2)
+  fb=f0-1.0*baud
+  ib=max(1,nint(fb/df))
+  k=0
+  c1=0.
+  do i=ib,it
+   c1(k)=cx(i)
+   k=k+1
+  enddo
+  c1=cshift(c1,i0-ib)
+  call four2a(c1,NFFT2,1,1,1)            !c2c FFT back to time domain
+  fac=1.0/sqrt(float(NFFT1)*NFFT2)
+  c1=fac*c1
+
+  return
+end subroutine ft8_downsample
---
+++ lib/fsk4hf/sync8d.f90
@@ -0,0 +1,54 @@
+subroutine sync8d(cd0,i0,ctwk,itwk,sync)
+
+! Compute sync power for a complex, downsampled FT8 signal.
+
+  parameter(NP2=2812)
+  complex cd0(3125)
+  complex csync(0:6,32)
+  complex csync2(32)
+  complex ctwk(32)
+  complex z1,z2,z3
+  logical first
+  integer icos7(0:6)
+  data icos7/2,5,6,0,4,1,3/
+  data first/.true./
+  save first,twopi,fs2,dt2,taus,baud,csync
+
+  p(z1)=real(z1)**2 + aimag(z1)**2          !Statement function for power
+
+! Set some constants and compute the csync array.  
+  if( first ) then
+    twopi=8.0*atan(1.0)
+    fs2=12000.0/64.0                        !Sample rate after downsampling
+    dt2=1/fs2                               !Corresponding sample interval
+    taus=32*dt2                             !Symbol duration
+    baud=1/taus                             !Keying rate
+    do i=0,6
+      phi=0.0
+      dphi=twopi*icos7(i)*baud*dt2  
+      do j=1,32
+        csync(i,j)=cmplx(cos(phi),sin(phi)) !Waveform for 7x7 Costas array
+        phi=mod(phi+dphi,twopi)
+      enddo
+    enddo
+    first=.false.
+  endif
+
+  sync=0
+  do i=0,6                              !Sum over 7 Costas frequencies and
+     i1=i0+i*32                         !three Costas arrays
+     i2=i1+36*32
+     i3=i1+72*32
+     csync2=csync(i,1:32)
+     if(itwk.eq.1) csync2=ctwk*csync2      !Tweak the frequency
+     z1=0.
+     z2=0.
+     z3=0.
+     if(i1.ge.1 .and. i1+31.le.NP2) z1=sum(cd0(i1:i1+31)*conjg(csync2))
+     if(i2.ge.1 .and. i2+31.le.NP2) z2=sum(cd0(i2:i2+31)*conjg(csync2))
+     if(i3.ge.1 .and. i3+31.le.NP2) z3=sum(cd0(i3:i3+31)*conjg(csync2))
+     sync = sync + p(z1) + p(z2) + p(z3)
+  enddo
+
+  return
+end subroutine sync8d
