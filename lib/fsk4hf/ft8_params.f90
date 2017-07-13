6e8ebd10244c8823f1b0d1094dd353a304c5c7dd
Steven Franke <s.j.franke@icloud.com>
2017-07-13 00:28:57 +0000

    Fix NHSYM.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@7862 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- lib/fsk4hf/ft8_params.f90
+++ lib/fsk4hf/ft8_params.f90
@@ -8,5 +8,5 @@ parameter (NZ=NSPS*NN)                !Samples in full 15 s waveform (151,680)
 parameter (NMAX=15*12000)             !Samples in iwave (180,000)
 parameter (NFFT1=2*NSPS, NH1=NFFT1/2) !Length of FFTs for symbol spectra
 parameter (NSTEP=NSPS/4)              !Rough time-sync step size
-parameter (NHSYM=NMAX/NSTEP-1)        !Number of symbol spectra (1/2-sym steps)
+parameter (NHSYM=NMAX/NSTEP-3)        !Number of symbol spectra (1/2-sym steps)
 parameter (NDOWN=60)                  !Downsample factor
