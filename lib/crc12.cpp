29fc1719a9fd2e6ca34d7db86fd7d9b1c0ee9650
Bill Somerville <g4wjs@classdesign.com>
2017-07-16 14:32:28 +0000

    Improve support for older C++ compilers
    Some current  Linux distributions  are still using  GCC 4.6  which has
    no/poor  C++11 support.  probably more  of this  needed if  we are  to
    support such compilers.
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@7896 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- lib/crc12.cpp
+++ lib/crc12.cpp
@@ -1,4 +1,5 @@
 #include <boost/crc.hpp>
+#include <boost/config.hpp>
 
 extern "C"
 {
@@ -6,19 +7,25 @@ extern "C"
    bool crc12_check (unsigned char const * data, int length);
 }
 
+#define POLY 0xc06
+
+#ifdef BOOST_NO_CXX11_CONSTEXPR
+#define TRUNCATED_POLYNOMIAL POLY
+#else
 namespace
 {
-  unsigned long constexpr truncated_polynomial = 0xc06;
+  unsigned long constexpr TRUNCATED_POLYNOMIAL = POLY;
 }
+#endif
 
 // assumes CRC is last 16 bits of the data and is set to zero
 // caller should assign the returned CRC into the message in big endian byte order
 short crc12 (unsigned char const * data, int length)
 {
-    return boost::augmented_crc<12, truncated_polynomial> (data, length);
+    return boost::augmented_crc<12, TRUNCATED_POLYNOMIAL> (data, length);
 }
 
 bool crc12_check (unsigned char const * data, int length)
 {
-   return !boost::augmented_crc<12, truncated_polynomial> (data, length);
+   return !boost::augmented_crc<12, TRUNCATED_POLYNOMIAL> (data, length);
 }
