bf110bd5b64c449a1818227d4ca3f92335ecdd86
Joe Taylor <k1jt@arrl.org>
2017-09-30 19:25:01 +0000

    Fix behavior with double-click on 'CQ <AA-ZZ> <call> <grid>.'
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@8147 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3830,6 +3830,7 @@ void MainWindow::processMessage(DecodedText const& message, bool shift, bool ctr
   }
 
   QString firstcall = message.call();
+  if(firstcall.length()==5 and firstcall.mid(0,3)=="CQ ") firstcall="CQ";
   if(!m_bFastMode and (!m_config.enable_VHF_features() or m_mode=="FT8")) {
     // Don't change Tx freq if in a fast mode, or VHF features enabled; also not if a
     // station is calling me, unless CTRL or SHIFT is held down.
