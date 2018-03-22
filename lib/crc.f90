8b164ba17f6077d8c4adf801a6806b24e2830e59
Steven Franke <s.j.franke@icloud.com>
2018-03-22 14:18:14 +0000

    Add crc14.cpp
    git-svn-id: svn+ssh://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx@8584 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- lib/crc.f90
+++ lib/crc.f90
@@ -1,6 +1,23 @@
 module crc 
   use, intrinsic :: iso_c_binding, only: c_int, c_loc, c_int8_t, c_bool, c_short
   interface
+
+    function crc14 (data, length) bind (C, name="crc14")
+      use, intrinsic :: iso_c_binding, only: c_short, c_ptr, c_int
+      implicit none
+      integer (c_short) :: crc14
+      type (c_ptr), value :: data
+      integer (c_int), value :: length
+    end function crc14
+ 
+    function crc14_check (data, length) bind (C, name="crc16_check")
+      use, intrinsic :: iso_c_binding, only: c_bool, c_ptr, c_int
+      implicit none
+      logical (c_bool) :: crc14_check
+      type (c_ptr), value :: data
+      integer (c_int), value :: length
+    end function crc14_check
+
     function crc12 (data, length) bind (C, name="crc12")
       use, intrinsic :: iso_c_binding, only: c_short, c_ptr, c_int
       implicit none
