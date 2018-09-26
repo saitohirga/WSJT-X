a09385604a305049765a01cc3a6ebbfc4afec9ed
Steve Franke <s.j.franke@icloud.com>
2018-09-26 08:30:41 -0500

    Fix a typo - thanks NU0C.

--- lib/crc.f90
+++ lib/crc.f90
@@ -26,7 +26,7 @@ module crc
       integer (c_int), value :: length
     end function crc13
  
-    function crc13_check (data, length) bind (C, name="crc14_check")
+    function crc13_check (data, length) bind (C, name="crc13_check")
       use, intrinsic :: iso_c_binding, only: c_bool, c_ptr, c_int
       implicit none
       logical (c_bool) :: crc13_check
