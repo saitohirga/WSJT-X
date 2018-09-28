77bb49e9ac27327b5e17928142ab85fb43c1ef8d
Steve Franke <s.j.franke@icloud.com>
2018-09-28 14:13:58 -0500

    Beginnings of AP for RU. Not working yet.

--- lib/ft8/ft8apset_174_91.f90
+++ lib/ft8/ft8apset_174_91.f90
@@ -1,20 +1,22 @@
-subroutine ft8apset_174_91(mycall12,hiscall12,hisgrid6,ncontest,apsym)
+subroutine ft8apset_174_91(mycall12,hiscall12,ncontest,apsym)
   parameter(NAPM=4,KK=91)
   character*37 msg,msgsent
-  character*12 mycall12,hiscall12
-  character*6 hisgrid6
-  character*4 hisgrid
+  character*12 mycall12,hiscall12,hiscall
   integer apsym(77)
   integer*1 msgbits(77)
   integer itone(KK)
-  
-  if(index(hiscall12," ").eq.0) hiscall12="K9ABC"
-  msg=trim(mycall12)//' '//trim(hiscall12)//' RRR' 
-  i3=1 
-  n3=0
-!write(*,*) 'apset msg ',msg
+ 
+  hiscall=hiscall12 
+  if(len(trim(hiscall)).eq.0) hiscall="K9ABC"
+  if(ncontest.eq.0) then
+     msg=trim(mycall12)//' '//trim(hiscall)//' RRR' 
+  elseif(ncontest.eq.4) then
+     msg=trim(mycall12)//' '//trim(hiscall)//' 599 NJ' 
+  endif 
+! write(*,*) 'apset msg ',msg
   call genft8_174_91(msg,i3,n3,msgsent,msgbits,itone)
+! write(*,*) 'apset msg sent',msgsent
   apsym=2*msgbits-1
-!write(*,'(29i1,1x,29i1,1x,19i1)') (apsym(1:77)+1)/2
+! write(*,'(29i1,1x,29i1,1x,19i1)') (apsym(1:77)+1)/2
   return
 end subroutine ft8apset_174_91
