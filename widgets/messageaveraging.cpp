552df2d520078e8778722d1cce21bfec1f789d72
Bill Somerville <bill@classdesign.com>
2018-11-12 22:03:39 +0000

    Revert the message averaging window to it's original single personality

--- widgets/messageaveraging.cpp
+++ widgets/messageaveraging.cpp
@@ -14,33 +14,16 @@ MessageAveraging::MessageAveraging(QSettings * settings, QFont const& font, QWid
   ui(new Ui::MessageAveraging)
 {
   ui->setupUi(this);
-//  setWindowTitle (QApplication::applicationName () + " - " + tr ("Message Averaging"));
+  setWindowTitle (QApplication::applicationName () + " - " + tr ("Message Averaging"));
   ui->msgAvgPlainTextEdit->setReadOnly (true);
   changeFont (font);
   read_settings ();
-  if(m_title_.contains("Fox")) {
-    ui->header_label->setText("   Date     Time   Call Grid Sent Rcvd Band");
-  } else if(m_title_.contains("Contest")) {
-    ui->header_label->setText("    Date    UTC   Band Call          Sent          Rcvd");
-    ui->lab1->setText("QSOs: 0");
-    ui->lab2->setText("Mults: 0");
-    ui->lab3->setText("Score: 0");
-    ui->lab4->setText("Rate: 0");
-  } else {
-    ui->header_label->setText("   UTC  Sync    DT  Freq   ");
-    ui->lab1->setVisible(false);
-    ui->lab2->setVisible(false);
-    ui->lab3->setVisible(false);
-    ui->lab4->setVisible(false);
-  }
-
-  setWindowTitle(m_title_);
-  m_nLogged_=0;
+  ui->header_label->setText("   UTC  Sync    DT  Freq   ");
 }
 
 MessageAveraging::~MessageAveraging()
 {
-  if (isVisible ()) write_settings ();
+  write_settings ();
 }
 
 void MessageAveraging::changeFont (QFont const& font)
@@ -70,90 +53,19 @@ void MessageAveraging::setContentFont(QFont const& font)
   ui->msgAvgPlainTextEdit->ensureCursorVisible ();
 }
 
-void MessageAveraging::closeEvent (QCloseEvent * e)
-{
-  write_settings ();
-  QWidget::closeEvent (e);
-}
-
 void MessageAveraging::read_settings ()
 {
   SettingsGroup group {settings_, "MessageAveraging"};
   restoreGeometry (settings_->value ("window/geometry").toByteArray ());
-  m_title_=settings_->value("window/title","Message Averaging").toString();
-  m_nContest_=settings_->value("nContest",0).toInt();
 }
 
 void MessageAveraging::write_settings ()
 {
   SettingsGroup group {settings_, "MessageAveraging"};
   settings_->setValue ("window/geometry", saveGeometry ());
-  settings_->setValue("window/title",m_title_);
-  settings_->setValue("nContest",m_nContest_);
 }
 
 void MessageAveraging::displayAvg(QString const& t)
 {
   ui->msgAvgPlainTextEdit->setPlainText(t);
-}
-
-void MessageAveraging::foxLogSetup(int nContest)
-{
-  if(nContest==5) {
-    m_title_=QApplication::applicationName () + " - Fox Log";
-    setWindowTitle(m_title_);
-    ui->header_label->setText("   Date    Time  Call    Grid Sent Rcvd Band");
-  }
-  if(nContest>0 and nContest<5) {
-    m_title_=QApplication::applicationName () + " - Contest Log";
-    setWindowTitle(m_title_);
-    ui->header_label->setText("    Date    UTC   Band Call          Sent          Rcvd");
-  }
-  m_nContest_=nContest;
-}
-
-void MessageAveraging::foxLabCallers(int n)
-{
-  QString t;
-  t.sprintf("Callers: %3d",n);
-  ui->lab1->setText(t);
-}
-
-void MessageAveraging::foxLabQueued(int n)
-{
-  QString t;
-  t.sprintf("In progress: %3d",n);
-  ui->lab2->setText(t);
-}
-
-void MessageAveraging::foxLabRate(int n)
-{
-  QString t;
-  t.sprintf("Rate: %3d",n);
-  ui->lab4->setText(t);
-}
-
-void MessageAveraging::foxAddLog(QString logLine)
-{
-  ui->msgAvgPlainTextEdit->appendPlainText(logLine);
-  m_nLogged_++;
-  QString t;
-  t.sprintf("Logged: %d",m_nLogged_);
-  ui->lab3->setText(t);
-}
-
-void MessageAveraging::contestAddLog(qint32 nContest, QString logLine)
-{
-  m_nContest_=nContest;
-  ui->msgAvgPlainTextEdit->appendPlainText(logLine);
-  m_nLogged_++;
-  QString t;
-  t.sprintf("QSOs: %d",m_nLogged_);
-  ui->lab1->setText(t);
-  if(m_mult_<1) m_mult_=1;
-  t.sprintf("Mults: %d",m_mult_);
-  ui->lab2->setText(t);
-  int score=m_mult_*m_nLogged_;
-  t.sprintf("Score: %d",score);
-  ui->lab3->setText(t);
 }
--- widgets/messageaveraging.h
+++ widgets/messageaveraging.h
@@ -1,5 +1,6 @@
-#ifndef MESSAGEAVERAGING_H
-#define MESSAGEAVERAGING_H
+// -*- Mode: C++ -*-
+#ifndef MESSAGEAVERAGING_H_
+#define MESSAGEAVERAGING_H_
 
 #include <QWidget>
 
@@ -10,34 +11,22 @@ namespace Ui {
   class MessageAveraging;
 }
 
-class MessageAveraging : public QWidget
+class MessageAveraging
+  : public QWidget
 {
 public:
   explicit MessageAveraging(QSettings *, QFont const&, QWidget * parent = 0);
   ~MessageAveraging();
   void displayAvg(QString const&);
   void changeFont (QFont const&);
-  void foxLogSetup(int nContest);
-  void foxLabCallers(int n);
-  void foxLabQueued(int n);
-  void foxLabRate(int n);
-  void foxAddLog(QString logLine);
-  void contestAddLog(qint32 nContest, QString logLine);
-
-protected:
-  void closeEvent (QCloseEvent *) override;
 
 private:
   void read_settings ();
   void write_settings ();
   void setContentFont (QFont const&);
   QSettings * settings_;
-  QString m_title_;
-  qint32  m_nLogged_=0;
-  qint32  m_mult_=0;
-  qint32  m_nContest_;
 
   QScopedPointer<Ui::MessageAveraging> ui;
 };
 
-#endif // MESSAGEAVERAGING_H
+#endif
