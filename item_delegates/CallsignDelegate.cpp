97431d815435c8dca88ec313a50c49ad67795734
Bill Somerville <bill@classdesign.com>
2018-11-12 02:00:35 +0000

    Add some item delagate classes useful for database fields

---
+++ item_delegates/CallsignDelegate.cpp
@@ -0,0 +1,30 @@
+#include "CallsignDelegate.hpp"
+
+#include <QLineEdit>
+
+#include "validators/CallsignValidator.hpp"
+
+CallsignDelegate::CallsignDelegate (QObject * parent)
+  : QStyledItemDelegate {parent}
+  , validator_ {new CallsignValidator}
+{
+}
+
+QWidget * CallsignDelegate::createEditor (QWidget * parent, QStyleOptionViewItem const&
+                                          , QModelIndex const&) const
+{
+  auto * editor = new QLineEdit {parent};
+  editor->setFrame (false);
+  editor->setValidator (validator_.data ());
+  return editor;
+}
+
+void CallsignDelegate::setEditorData (QWidget * editor, QModelIndex const& index) const
+{
+  static_cast<QLineEdit *> (editor)->setText (index.model ()->data (index, Qt::EditRole).toString ());
+}
+
+void CallsignDelegate::setModelData (QWidget * editor, QAbstractItemModel * model, QModelIndex const& index) const
+{
+  model->setData (index, static_cast<QLineEdit *> (editor)->text (), Qt::EditRole);
+}
