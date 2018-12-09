205024f8effecc7b5a77a6e159df2169710bbfbc
Bill Somerville <bill@classdesign.com>
2018-12-09 21:18:08 +0000

    GA release and make initial message show in GA version

--- Versions.cmake
+++ Versions.cmake
@@ -2,5 +2,5 @@
 set (WSJTX_VERSION_MAJOR 2)
 set (WSJTX_VERSION_MINOR 0)
 set (WSJTX_VERSION_PATCH 0)
-set (WSJTX_RC 6)		 # release candidate number, comment out or zero for development versions
-set (WSJTX_VERSION_IS_RELEASE 0) # set to 1 for final release build
+#set (WSJTX_RC 6)		 # release candidate number, comment out or zero for development versions
+set (WSJTX_VERSION_IS_RELEASE 1) # set to 1 for final release build
