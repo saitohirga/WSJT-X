4e9d3e1076a76b1ebb2f2d387e405eec2c39c685
Bill Somerville <bill@classdesign.com>
2020-06-01 16:48:50 +0100

    Remove RC expiration and nag message

--- widgets/mainwindow.cpp
+++ widgets/mainwindow.cpp
@@ -996,14 +996,14 @@ MainWindow::MainWindow(QDir const& temp_directory, bool multiple,
 
 void MainWindow::not_GA_warning_message ()
 {
-  MessageBox::critical_message (this,
-                                "This is a pre-release version of WSJT-X 2.2.0 made\n"
-                                "available for testing purposes.  By design it will\n"
-                                "be nonfunctional after 0000 UTC on June 10, 2020.");
-  auto now = QDateTime::currentDateTimeUtc ();
-  if (now >= QDateTime {{2020, 6, 10}, {0, 0}, Qt::UTC}) {
-    Q_EMIT finished ();
-  }
+  // MessageBox::critical_message (this,
+  //                               "This is a pre-release version of WSJT-X 2.2.0 made\n"
+  //                               "available for testing purposes.  By design it will\n"
+  //                               "be nonfunctional after 0000 UTC on June 10, 2020.");
+  // auto now = QDateTime::currentDateTimeUtc ();
+  // if (now >= QDateTime {{2020, 6, 10}, {0, 0}, Qt::UTC}) {
+  //   Q_EMIT finished ();
+  // }
 }
 
 void MainWindow::initialize_fonts ()
