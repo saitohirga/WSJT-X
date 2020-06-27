3cb1980ef1b730e0a36680f4ccd0b2df860d6c89
Steven Franke <s.j.franke@icloud.com>
2020-06-27 09:21:43 -0500

    Remove un-needed files.

--- lib/fst240/fst280_params.f90
+++
@@ -1,8 +0,0 @@
-! FT4S280
-! LDPC(280,101)/CRC24 code, six 4x4 Costas arrays for sync, ramp-up and ramp-down symbols
-
-parameter (KK=77)                     !Information bits (77 + CRC24)
-parameter (ND=140)                    !Data symbols
-parameter (NS=24)                     !Sync symbols 
-parameter (NN=NS+ND)                  !Sync and data symbols (164)
-parameter (NN2=NS+ND+2)               !Total channel symbols (166)
