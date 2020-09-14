ca0804450baf56378346d65727dfa5447d86c172
Steven Franke <s.j.franke@icloud.com>
2020-09-14 09:07:45 -0500

    Remove some redundant code.

--- lib/fst4_decode.f90
+++ lib/fst4_decode.f90
@@ -214,15 +214,12 @@ contains
       if(ndepth.eq.3) then
          nblock=4
          jittermax=2
-         norder=3
       elseif(ndepth.eq.2) then
          nblock=3
          jittermax=0
-         norder=3
       elseif(ndepth.eq.1) then
          nblock=1
          jittermax=0
-         norder=3
       endif
 
       ndropmax=1
