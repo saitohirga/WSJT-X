c9fb6510a0d1ccbe4cb6e5b58e4c33bb937f5d05
Bill Somerville <bill@classdesign.com>
2021-01-04 22:50:02 +0000

    Update expiry and nag message

--- widgets/mainwindow.cpp
+++ widgets/mainwindow.cpp
@@ -1039,9 +1039,9 @@ void MainWindow::not_GA_warning_message ()
   MessageBox::critical_message (this,
                                 "This is a pre-release version of WSJT-X 2.3.0 made\n"
                                 "available for testing purposes.  By design it will\n"
-                                "be nonfunctional after 0000 UTC on Jan 19, 2021.");
+                                "be nonfunctional after 0000 UTC on Jan 26, 2021.");
   auto now = QDateTime::currentDateTimeUtc ();
-  if (now >= QDateTime {{2021, 1, 19}, {0, 0}, Qt::UTC}) {
+  if (now >= QDateTime {{2021, 1, 26}, {0, 0}, Qt::UTC}) {
     Q_EMIT finished ();
   }
 }
