1c038ef979c2283b841c0eed29a75ca54260d9bc
Joe Taylor <joe@princeton.edu>
2021-02-06 13:07:01 -0500

    Remove the "Call 1st" checkbox from the Q65 GUI.  It's not used.

--- widgets/mainwindow.cpp
+++ widgets/mainwindow.cpp
@@ -6427,7 +6427,7 @@ void MainWindow::on_actionQ65_triggered()
   m_wideGraph->setTxFreq(ui->TxFreqSpinBox->value());
   switch_mode (Modes::Q65);
 //                         0123456789012345678901234567890123456
-  displayWidgets(nWidgets("1111110101101101001110000001000000001"));
+  displayWidgets(nWidgets("1111110101101101001110000000000000001"));
   ui->lh_decodes_title_label->setText(tr ("Single-Period Decodes"));
   ui->rh_decodes_title_label->setText(tr ("Average Decodes"));
   ui->lh_decodes_headings_label->setText("UTC   dB   DT Freq    " + tr ("Message"));
