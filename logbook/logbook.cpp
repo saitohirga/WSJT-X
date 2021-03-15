1dc850d351ea6cdf20cb99bdfcda79aa3a8abb79
Bill Somerville <bill@classdesign.com>
2021-03-15 23:29:03 +0000

    Align ADIF MODE/SUBMODE for Q65 with proposed ADIF specification

--- logbook/logbook.cpp
+++ logbook/logbook.cpp
@@ -79,7 +79,7 @@ QByteArray LogBook::QSOToADIF (QString const& hisCall, QString const& hisGrid, Q
   QString t;
   t = "<call:" + QString::number(hisCall.size()) + ">" + hisCall;
   t += " <gridsquare:" + QString::number(hisGrid.size()) + ">" + hisGrid;
-  if (mode != "FT4" && mode != "FST4")
+  if (mode != "FT4" && mode != "FST4" && mode != "Q65")
     {
       t += " <mode:" + QString::number(mode.size()) + ">" + mode;
     }
