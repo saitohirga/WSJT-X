6dac83abe229eb3392f148f9e1a3e9dd6d657c95
Bill Somerville <bill@classdesign.com>
2021-04-13 14:18:26 +0100

    Temporary diagnstics

--- CMake/Modules/Findportaudio.cmake
+++ CMake/Modules/Findportaudio.cmake
@@ -10,6 +10,21 @@
 #   portaudio::portaudio	- The portaudio library
 #
 
+function(dump_cmake_variables)
+  get_cmake_property(_variableNames VARIABLES)
+  list (SORT _variableNames)
+  foreach (_variableName ${_variableNames})
+    if (ARGV0)
+      unset(MATCHED)
+      string(REGEX MATCH ${ARGV0} MATCHED ${_variableName})
+      if (NOT MATCHED)
+        continue()
+      endif()
+    endif()
+    message(STATUS "${_variableName}=${${_variableName}}")
+  endforeach()
+endfunction()
+
 include (LibFindMacros)
 
 libfind_pkg_detect (portaudio portaudio-2.0
@@ -18,15 +33,15 @@ libfind_pkg_detect (portaudio portaudio-2.0
   )
 
 libfind_process (portaudio)
-
+dump_cmake_variables ("portaudio")
 if (portaudio_FOUND AND NOT TARGET portaudio::portaudio)
   add_library (portaudio::portaudio UNKNOWN IMPORTED)
   set_target_properties (portaudio::portaudio PROPERTIES
-    IMPORTED_LOCATION "${portaudio_LIBRARY}"
-    INTERFACE_COMPILE_OPTIONS "${portaudio_PKGCONF_CFLAGS_OTHER}"
-    INTERFACE_INCLUDE_DIRECTORIES "${portaudio_INCLUDE_DIRS}"
-    INTERFACE_LINK_LIBRARIES "${portaudio_PKGCONF_LDFLAGS}"
+    IMPORTED_LOCATION ${portaudio_LIBRARY}
+    INTERFACE_COMPILE_OPTIONS ${portaudio_PKGCONF_CFLAGS_OTHER}
+    INTERFACE_INCLUDE_DIRECTORIES ${portaudio_INCLUDE_DIRS}
     )
+  target_link_libraries (portaudio::portaudio INTERFACE ${portaudio_PKGCONF_LDFLAGS})
 endif ()
 
 mark_as_advanced (
