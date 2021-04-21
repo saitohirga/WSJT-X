bc88592684339dea1426bec640863a727b3fe519
Joe Taylor <joe@princeton.edu>
2021-04-21 11:28:09 -0400

    Don't start decoder until t=56 s, to accommodate Q65 Tx duration and EME delay.

--- map65/mainwindow.cpp
+++ map65/mainwindow.cpp
@@ -630,7 +630,8 @@ void MainWindow::dataSink(int k)
     ntrz=ntr;
     n=0;
   }
-  if(ihsym == 280) {
+//if(ihsym == 280) {   //For JT65, decode at t=52 s
+  if(ihsym == 302) {   //For Q65, decode at t=56 s
     datcom_.newdat=1;
     datcom_.nagain=0;
     QDateTime t = QDateTime::currentDateTimeUtc();
