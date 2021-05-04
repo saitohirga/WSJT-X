3f74137d320abe988fe44dc1a5b286bb2b3ad583
Joe Taylor <joe@princeton.edu>
2021-05-04 15:46:00 -0400

    Add Q65 modes and optional Doppler spread to mapsim.

---
+++ map65/libm65/gen_q65_cwave.f90
@@ -0,0 +1,70 @@
+subroutine gen_q65_cwave(msg,ntxfreq,ntone_spacing,msgsent,cwave,nwave)
+
+! Encodes a Q65 message to yield complex cwave() at fsample = 96000 Hz
+
+  use packjt
+  parameter (NMAX=60*96000)
+  character*22 msg
+  character*22 msgsent          !Message as it will be received
+  character*120 cmnd
+  character*80 wsjtx_dir
+  character*16 cjunk
+  real*8 t,dt,phi,f,f0,dfgen,dphi,twopi,tsym
+  complex cwave(NMAX)
+  integer itone(85)
+  integer icos7(0:6)
+  data icos7/2,5,6,0,4,1,3/     !Defines a 7x7 Costas array
+  data twopi/6.283185307179586476d0/
+  save
+
+  open(9,file='wsjtx_dir.txt',status='old')
+  read(9,'(a)') wsjtx_dir
+  close(9)
+
+  msgsent=msg
+!                1         2         3         4         5
+!       12345678901234567890123456789012345678901234567890123456789012345
+  cmnd='q65sim "K1ABC W9XYZ EN37      " A 1500 0 0 0 0 60 0 99 >itone.txt'
+  cmnd(9:30)=msg
+  write(cmnd(35:38),'(i4)') ntxfreq
+  cmnd=trim(wsjtx_dir)//cmnd
+  call execute_command_line(cmnd)
+  open(9,file='itone.txt',status='old')
+  do i=1,99
+     read(9,1000,end=999) cjunk
+1000 format(a16)
+     if(cjunk.eq.'Channel symbols:') exit
+  enddo
+  read(9,1002) itone
+1002 format(20i3)
+  close(9)
+
+! Set up necessary constants
+  nsym=85
+  tsym=7200.d0/12000.d0
+  dt=1.d0/96000.d0
+  f0=ntxfreq
+  dfgen=ntone_spacing*12000.d0/7200.d0
+  phi=0.d0
+  dphi=twopi*dt*f0
+  i=0
+  nwave=85*7200*96000.d0/12000.d0
+  t=0.d0
+  j0=0
+  do i=1,nwave
+     t=t+dt
+     j=t/tsym + 1
+     if(j.gt.85) exit
+     if(j.ne.j0) then
+        f=f0 + itone(j)*dfgen
+        dphi=twopi*dt*f
+        j0=j
+     endif
+     phi=phi+dphi
+     if(phi.gt.twopi) phi=phi-twopi
+     xphi=phi
+     cwave(i)=cmplx(cos(xphi),-sin(xphi))
+  enddo
+
+999  return
+end subroutine gen_q65_cwave
--- map65/libm65/mapsim.f90
+++ map65/libm65/mapsim.f90
@@ -2,7 +2,7 @@ program mapsim
 
 ! Generate simulated data for testing of MAP65
 
-  parameter (NMAX=96000*60)
+  parameter (NMAX=60*96000)
   real*4 d4(4,NMAX)                   !Floating-point data
   integer*2 id4(4,NMAX)               !i*2 data, dual polarization
   integer*2 id2(2,NMAX)               !i*2 data, single polarization
@@ -10,35 +10,41 @@ program mapsim
   complex z,zx,zy
   real*8 fcenter,fsample,samfac,f,dt,twopi,phi,dphi
   character msg0*22,message*22,msgsent*22,arg*8,fname*13,mode*2
+  logical bq65
 
   nargs=iargc()
-  if(nargs.ne.9) then
-     print*,'Usage: mapsim  DT "message"     mode f1 f2 nsigs pol SNR nfiles'
-     print*,'Example:      2.5 "CQ K1ABC FN42" B -22 33  20    45 -20    1'
+  if(nargs.ne.10) then
+     print*,'Usage:   mapsim "message"     mode DT  fa fb nsigs pol fDop SNR nfiles'
+     print*,'Example: mapsim "CQ K1ABC FN42" B 2.5 -20 20  21    45  0.0 -20   1'
      print*,' '
-     print*,'Enter message = "" to use entries in msgs.txt.'
-     print*,'Enter pol = -1 to generate a range of polarization angles.'
-     print*,'Enter SNR = 0 to generate a range of SNRs.'
+     print*,'         mode = A B C for JT65; QA-QE for Q65-60A' 
+     print*,'         fa = lowest freq in kHz, relative to center'
+     print*,'         fb = highest freq in kHz, relative to center'
+     print*,'         message = "" to use entries in msgs.txt.'
+     print*,'         pol = -1 to generate a range of polarization angles.'
+     print*,'         SNR = 0 to generate a range of SNRs.'
      go to 999
   endif
 
-  call getarg(1,arg)
-  read(arg,*) dt0                    !Time delay
-  call getarg(2,msg0)
+  call getarg(1,msg0)
   message=msg0                       !Transmitted message
-  call getarg(3,mode)                !JT65 sub-mode (A B C B2 C2)
+  call getarg(2,mode)                !JT65 sub-mode (A B C QA-QE)
+  call getarg(3,arg)
+  read(arg,*) dt0                    !Time delay
   call getarg(4,arg)
-  read(arg,*) f1                     !Lowest freq (kHz, relative to fcenter)
+  read(arg,*) fa                     !Lowest freq (kHz, relative to fcenter)
   call getarg(5,arg)
-  read(arg,*) f2                     !Highest freq
+  read(arg,*) fb                     !Highest freq
   call getarg(6,arg)
   read(arg,*) nsigs                  !Number of signals in each file
   call getarg(7,arg)
   read(arg,*) npol                   !Polarization in degrees
-  call getarg(8,arg)
-  read(arg,*) snrdb                  !S/N
   pol=npol
+  call getarg(8,arg)
+  read(arg,*) fdop                   !Doppler spread
   call getarg(9,arg)
+  read(arg,*) snrdb                  !S/N
+  call getarg(10,arg)
   read(arg,*) nfiles                 !Number of files
 
   rmsdb=25.
@@ -49,17 +55,20 @@ program mapsim
   twopi=8.d0*atan(1.d0)
   rad=360.0/twopi
   samfac=1.d0
-  mode65=1
-  if(mode(1:1).eq.'B') mode65=2
-  if(mode(1:1).eq.'C') mode65=4
-  nfast=1
-  if(mode(2:2).eq.'2') nfast=2
-  npts=NMAX/nfast
+  bq65=(mode(1:1).eq.'Q')
+  ntone_spacing=1
+  ntxfreq=1270
+  fac=1.0/32767.0
+  if(mode(1:1).eq.'B' .or. mode(2:2).eq.'B') ntone_spacing=2
+  if(mode(1:1).eq.'C' .or. mode(2:2).eq.'C') ntone_spacing=4
+  if(mode(2:2).eq.'D') ntone_spacing=8
+  if(mode(2:2).eq.'E') ntone_spacing=16
+  npts=NMAX
   open(12,file='msgs.txt',status='old')
 
   write(*,1000)
-1000 format('File  N   freq     S/N  pol  Message'/    &
-            '---------------------------------------------------')
+1000 format('File N Mode  DT    freq   pol   fDop   SNR   Message'/    &
+            '--------------------------------------------------------------------')
 
   do ifile=1,nfiles
      nmin=ifile-1
@@ -72,31 +81,43 @@ program mapsim
      call noisegen(d4,npts)                      !Generate Gaussuian noise
 
      if(msg0.ne.'                      ') then
-        call cgen65(message,mode65,nfast,samfac,nsendingsh,msgsent,cwave,nwave)
+        if(bq65) then
+           call gen_q65_cwave(message,ntxfreq,ntone_spacing,msgsent,      &
+                cwave,nwave)
+        else
+           call cgen65(message,ntone_spacing,samfac,nsendingsh,msgsent,  &
+                cwave,nwave)
+        endif
      endif
-
      rewind 12
+
+     if(fdop.gt.0.0) call dopspread(cwave,nwave,fdop)
+
      do isig=1,nsigs
 
         if(msg0.eq.'                      ') then
            read(12,1004) message
 1004       format(a22)
-           call cgen65(message,mode65,nfast,samfac,nsendingsh,msgsent,    &
-                cwave,nwave)
+           if(bq65) then
+           call gen_q65_cwave(msg,ntxfreq,mode65,msgsent,cwave,nwave)
+           else
+              call cgen65(message,ntone_spacing,samfac,nsendingsh,msgsent,    &
+                   cwave,nwave)
+           endif
         endif
-           
+
         if(npol.lt.0) pol=(isig-1)*180.0/nsigs
         a=cos(pol/rad)
         b=sin(pol/rad)
-        f=1000.0*(f1+f2)/2.0
-        if(nsigs.gt.1) f=1000.0*(f1 + (isig-1)*(f2-f1)/(nsigs-1.0))
+        f=1000.0*(fa+fb)/2.0
+        if(nsigs.gt.1) f=1000.0*(fa + (isig-1)*(fb-fa)/(nsigs-1.0))
         dphi=twopi*f*dt + 0.5*twopi
 
         snrdbx=snrdb
         if(snrdb.ge.-1.0) snrdbx=-15.0 - 15.0*(isig-1.0)/nsigs
         sig=sqrt(2.2*2500.0/96000.0) * 10.0**(0.05*snrdbx)
-        write(*,1020) ifile,isig,0.001*f,snrdbx,nint(pol),msgsent
-1020    format(i3,i4,f8.3,f7.1,i5,2x,a22)
+        write(*,1020) ifile,isig,mode,dt0,0.001*f,nint(pol),fDop,snrdbx,msgsent
+1020    format(i3,i3,2x,a2,f6.2,f8.3,i5,2f7.1,2x,a22)
 
         phi=0.
 !        i0=fsample*(3.5d0+0.05d0*(isig-1))
@@ -133,3 +154,55 @@ program mapsim
   enddo
 
 999 end program mapsim
+
+subroutine dopspread(cwave,nwave,fspread)
+
+  parameter (NMAX=60*96000)
+  parameter (NFFT=NMAX,NH=NFFT/2)
+  complex cwave(NMAX)
+  complex cspread(0:NMAX-1)
+
+  twopi=8.0*atan(1.0)
+  df=96000.0/nfft
+  cspread(0)=1.0
+  cspread(NH)=0.
+  b=6.0                       !Use truncated Lorenzian shape for fspread
+  do i=1,NH
+     f=i*df
+     x=b*f/fspread
+     z=0.
+     a=0.
+     if(x.lt.3.0) then                          !Cutoff beyond x=3
+        a=sqrt(1.111/(1.0+x*x)-0.1)             !Lorentzian amplitude
+        phi1=twopi*rran()                       !Random phase
+        z=a*cmplx(cos(phi1),sin(phi1))
+     endif
+     cspread(i)=z
+     z=0.
+     if(x.lt.3.0) then                !Same thing for negative freqs
+        phi2=twopi*rran()
+        z=a*cmplx(cos(phi2),sin(phi2))
+     endif
+     cspread(nfft-i)=z
+  enddo
+
+  call four2a(cspread,nfft,1,1,1)             !Transform to time domain
+
+  sum=0.
+  do i=0,nfft-1
+     p=real(cspread(i))**2 + aimag(cspread(i))**2
+     sum=sum+p
+  enddo
+  avep=sum/nfft
+  fac=sqrt(1.0/avep)
+  cspread=fac*cspread                   !Normalize to constant avg power
+  cwave=cspread*cwave                   !Apply Rayleigh fading
+  
+!  do i=0,nfft-1
+!     p=real(cspread(i))**2 + aimag(cspread(i))**2
+!     write(14,3010) i,p,cspread(i)
+!3010 format(i8,3f12.6)
+!  enddo
+
+  return
+end subroutine dopspread
