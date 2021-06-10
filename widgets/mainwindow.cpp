57c4187b24b31c657e4b24d0997b467b72a34687
Joe Taylor <joe@princeton.edu>
2021-06-10 14:40:56 -0400

    Remove the redundant cone for txDuration.

--- widgets/mainwindow.cpp
+++ widgets/mainwindow.cpp
@@ -3807,32 +3807,8 @@ void MainWindow::guiUpdate()
   double txDuration;
 
   if(m_TRperiod==0) m_TRperiod=60.0;
-  txDuration=0.0;
-  if(m_modeTx=="FT4")  txDuration=1.0 + 105*576/12000.0;      // FT4
-  if(m_modeTx=="FT8")  txDuration=1.0 + 79*1920/12000.0;      // FT8
-  if(m_modeTx=="JT4")  txDuration=1.0 + 207.0*2520/11025.0;   // JT4
-  if(m_modeTx=="JT9")  txDuration=1.0 + 85.0*m_nsps/12000.0;  // JT9
-  if(m_modeTx=="JT65") txDuration=1.0 + 126*4096/11025.0;     // JT65
-  if(m_modeTx=="Q65")  {                                      // Q65
-    if(m_TRperiod==15) txDuration=0.5 + 85*1800/12000.0;
-    if(m_TRperiod==30) txDuration=0.5 + 85*3600/12000.0;
-    if(m_TRperiod==60) txDuration=1.0 + 85*7200/12000.0;
-    if(m_TRperiod==120) txDuration=1.0 + 85*16000/12000.0;
-    if(m_TRperiod==300) txDuration=1.0 + 85*41472/12000.0;
-  }
-  if(m_modeTx=="WSPR") txDuration=2.0 + 162*8192/12000.0;     // WSPR
-  if(m_modeTx=="FST4" or m_mode=="FST4W") {               //FST4, FST4W
-    if(m_TRperiod==15)  txDuration=1.0 + 160*720/12000.0;
-    if(m_TRperiod==30)  txDuration=1.0 + 160*1680/12000.0;
-    if(m_TRperiod==60)  txDuration=1.0 + 160*3888/12000.0;
-    if(m_TRperiod==120) txDuration=1.0 + 160*8200/12000.0;
-    if(m_TRperiod==300) txDuration=1.0 + 160*21504/12000.0;
-    if(m_TRperiod==900) txDuration=1.0 + 160*66560/12000.0;
-    if(m_TRperiod==1800) txDuration=1.0 + 160*134400/12000.0;
-  }
-  if(m_mode=="MSK144" or m_bFast9) {
-    txDuration=m_TRperiod-0.25; // JT9-fast, MSK144
-  }
+  txDuration=tx_duration(m_mode,m_TRperiod,m_nsps,m_bFast9);
+
 
   double tx1=0.0;
   double tx2=txDuration;
@@ -3854,15 +3830,11 @@ void MainWindow::guiUpdate()
   m_tRemaining=m_TRperiod - fmod(tsec,m_TRperiod);
 
   if(m_mode=="Echo") {
-    txDuration=2.4;
     tx1=0.0;
     tx2=txDuration;
     if(m_auto and m_s6>4.0) m_bEchoTxOK=true;
     if(m_transmitting) m_bEchoTxed=true;
   }
-
-  Q_ASSERT(txDuration == tx_duration(m_mode,m_TRperiod,m_nsps,m_bFast9));
-  Q_ASSERT(m_mode==m_modeTx);
 
   if(m_mode=="WSPR" or m_mode=="FST4W") {
     if(m_nseq==0 and m_ntr==0) {                   //Decide whether to Tx or Rx
