dbb9eca66e5f3443cb08c984f11c95028de1d999
Joe Taylor <joe@princeton.edu>
2021-09-14 14:09:54 -0400

    Allow msk144sim[.exe] to generate files with length 5, 10, 15, and 30 s.

--- lib/makepings.f90
+++ lib/makepings.f90
@@ -1,20 +1,19 @@
-subroutine makepings(pings,npts,width,sig)
+subroutine makepings(pings,nTRperiod,npts,width,sig)
 
   real pings(npts)
   real*8 t
-  real t0(14)
+  real t0(29)
 
-  iping0=-999
   dt=1.0/12000.0
-  do i=1,14
+  do i=1,nTRperiod-1
      t0(i)=i                            !Make pings at t=1, 2, ... 14 s.
   enddo
   amp=sig
 
   do i=1,npts
-     iping=min(max(1,i/12000),14)
+     iping=min(max(1,i/12000),nTRperiod-1)
      t=(i*dt-t0(iping))/width
-     if(t.lt.0.d0 .and. t.lt.10.0) then    !????
+     if(t.lt.0.d0 .or. t.gt.10.0) then
         fac=0.
      else
         fac=2.718*t*dexp(-t)
