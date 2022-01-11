fc40770960229af267dcc4028ad377cb4f00e836
Joe Taylor <joe@princeton.edu>
2022-01-11 11:54:36 -0500

    Revert "Improve ft8 decoder sensitivity for Normal and Deep settings"
    This reverts commit 3e49dfacc5c495492bb327e3cc8bdc42b98aa3f6.

--- lib/ft8_decode.f90
+++ lib/ft8_decode.f90
@@ -167,9 +167,8 @@ contains
     if(ndepth.eq.1) npass=1
     do ipass=1,npass
       newdat=.true.
-      syncmin=1.2
-      if(ndepth.eq.1) syncmin=1.6
-      if(ndepth.eq.2) syncmin=1.4
+      syncmin=1.3
+      if(ndepth.le.2) syncmin=1.6
       if(ipass.eq.1) then
         lsubtract=.true.
         ndeep=ndepth
