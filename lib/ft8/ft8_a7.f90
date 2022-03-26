6414a8624bcdded78af22a7945a383ca4c8524be
k9an <s.j.franke@icloud.com>
2022-03-26 18:44:59 -0500

    Make minimum SNR -24 dB for a7 decodes.

--- lib/ft8/ft8_a7.f90
+++ lib/ft8/ft8_a7.f90
@@ -364,7 +364,7 @@ subroutine ft8_a7d(dd0,newdat,call_1,call_2,grid4,xdt,f1,xbase,nharderrors,dmin,
   dmin2=dmm(iloc(1))
   xsnr=-24.
   arg=pbest/xbase/3.0e6-1.0
-  if(arg.gt.0.0) xsnr=db(arg)-27.0
+  if(arg.gt.0.0) xsnr=max(-24.0,db(arg)-27.0)
 !  write(41,3041) nharderrors,dmin,dmin2,dmin2/dmin,xsnr,trim(msgbest)
 !3041 format(i3,2f7.1,f7.2,f7.1,1x,a)
   if(dmin.gt.100.0 .or. dmin2/dmin.lt.1.3) nharderrors=-1
